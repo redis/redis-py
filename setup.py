3d9825388c9cbbb5ffbc300f42f7c47a564cbe42
Andy McCurdy <andy@andymccurdy.com>
2011-06-01 01:25:25 -0700

    long description is now the contents of the readme file

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
+import os
 from redis import __version__
+
+f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+long_description = f.read()
+f.close()
 
 sdict = {
     'name' : 'redis',
     'version' : __version__,
     'description' : 'Python client for Redis key-value store',
-    'long_description' : 'Python client for Redis key-value store',
+    'long_description' : long_description,
     'url': 'http://github.com/andymccurdy/redis-py',
     'download_url' : 'http://cloud.github.com/downloads/andymccurdy/redis-py/redis-%s.tar.gz' % __version__,
     'author' : 'Andy McCurdy',
@@ -29,6 +34,6 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
-    
+
 setup(**sdict)
 
