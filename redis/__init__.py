4103be272bd886498ec850ea7d5ce5c3b4979014
Andy McCurdy <andy@andymccurdy.com>
2011-06-07 05:35:42 -0700

    added VERSION as a tuple based on the __version__ string

--- redis/__init__.py
+++ redis/__init__.py
@@ -16,6 +16,7 @@ from redis.exceptions import (
 
 
 __version__ = '2.4.4'
+VERSION = tuple(map(int, __version__.split('.')))
 
 __all__ = [
     'Redis', 'ConnectionPool', 'Connection', 'UnixDomainSocketConnection',
