5403717ddb18b709e445cb91871bf6d1fe1787db
mardiros <mardiros@laposte.net>
2011-09-28 14:47:27 +0200

    add gevent socket support

---
+++ redis/gevent_connection.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from gevent import socket
+from . import connection
+
+class Connection(connection.Connection):
+    
+    def _connect(self):
+        "Create a TCP socket connection"
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(self.socket_timeout)
+        sock.connect((self.host, self.port))
+        return sock
