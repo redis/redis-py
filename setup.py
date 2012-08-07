309a2a9b655857fcb8453d142b3247aef1b7b49f
Alex Grönholm <alex.gronholm+git@nextday.fi>
2012-08-07 03:15:19 +0300

    Added classifiers to indicate supported Python versions

--- setup.py
+++ setup.py
@@ -34,5 +34,12 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python']
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        ]
 )
