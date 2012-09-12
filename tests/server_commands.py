910f4daa817f2c8bf7414a8823b91b4b4d82180f
Luper Rouch <luper.rouch@gmail.com>
2012-09-12 17:11:28 +0200

    Revert "added PEXPIRE/PTTL support (redis 2.6.0)"
    This reverts commit 65fd683593fb7642dc1c6c99b98a55ee29cdf212.

--- tests/server_commands.py
+++ tests/server_commands.py
@@ -168,21 +168,6 @@ class ServerCommandsTestCase(unittest.TestCase):
         self.assertEquals(self.client.persist('a'), True)
         self.assertEquals(self.client.ttl('a'), None)
 
-    def test_pexpire(self):
-        version = self.client.info()['redis_version']
-        if StrictVersion(version) < StrictVersion('2.6.0'):
-            try:
-                raise unittest.SkipTest()
-            except AttributeError:
-                return
-
-        self.assertEquals(self.client.pexpire('a', 10000), False)
-        self.client['a'] = 'foo'
-        self.assertEquals(self.client.expire('a', 10000), True)
-        self.assertEquals(self.client.pttl('a'), 10000)
-        self.assertEquals(self.client.persist('a'), True)
-        self.assertEquals(self.client.pttl('a'), None)
-
     def test_expireat(self):
         expire_at = datetime.datetime.now() + datetime.timedelta(minutes=1)
         self.assertEquals(self.client.expireat('a', expire_at), False)
