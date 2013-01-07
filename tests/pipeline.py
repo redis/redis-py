5827683b8d2be7bd060ab579d8ab4655e4820052
Jon Parise <jon@indelible.org>
2013-01-06 17:36:45 -0800

    Pipelines now have a __len__() method.
    The length of a pipeline is defined as the length of its command
    stack.  This makes it easy to inspect the number of batched commands
    and to write conditional logic for empty pipelines.

--- tests/pipeline.py
+++ tests/pipeline.py
@@ -28,6 +28,22 @@ class PipelineTestCase(unittest.TestCase):
                 ]
             )
 
+    def test_pipeline_length(self):
+        with self.client.pipeline() as pipe:
+            # Initially empty.
+            self.assertEquals(len(pipe), 0)
+            self.assertFalse(pipe)
+
+            # Fill 'er up!
+            pipe.set('a', 'a1').set('b', 'b1').set('c', 'c1')
+            self.assertEquals(len(pipe), 3)
+            self.assertTrue(pipe)
+
+            # Execute calls reset(), so empty once again.
+            pipe.execute()
+            self.assertEquals(len(pipe), 0)
+            self.assertFalse(pipe)
+
     def test_pipeline_no_transaction(self):
         with self.client.pipeline(transaction=False) as pipe:
             pipe.set('a', 'a1').set('b', 'b1').set('c', 'c1')
