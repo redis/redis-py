c9ce0d16480bc3b1041a0aff85d3ea49f1a55d8d
Antonio Cavallo <a.cavallo@cavallinux.eu>
2013-04-17 13:54:32 +0100

    added a missing function

--- redis/utils.py
+++ redis/utils.py
@@ -1,4 +1,6 @@
+import os
 from redis.client import Redis
+
 
 def from_url(url, db=None, **kwargs):
     """Returns an active Redis client generated from the given database URL.
@@ -7,3 +9,23 @@ def from_url(url, db=None, **kwargs):
     none is provided.
     """
     return Redis.from_url(url, db, **kwargs)
+
+
+def which(exe, raise_=True):
+    "returns the full pathname to exe"
+    result = exe
+    if not os.path.isabs(exe):
+        for sdir in os.environ.get("PATH", "").split(os.path.pathsep):
+            pname = os.path.join(sdir, exe)
+            if os.path.exists(pname):
+                result = pname
+                break
+    if os.path.exists(result):
+        return os.path.normpath(result)
+    if raise_ == True:
+        raise IOError(u"unable to find %s" % exe)
+    elif raise_ in [ False, None, ]:
+        return None
+    else:
+        raise raise_
+
