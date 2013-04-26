1d6e69290bb6a6febc9e59aa248a16255c43bc11
Antonio Cavallo <a.cavallo@cavallinux.eu>
2013-04-26 21:08:59 +0100

    fix pep8 compliance

--- tests/test_testserver.py
+++ tests/test_testserver.py
@@ -7,17 +7,60 @@ if not hasattr(unittest, "skipIf"):
     import unittest2 as unittest
 # end of compat layer
 
-
 import sys
 import os
 import logging
-
 
 import redis
 import redis.test_server
 
 
 FREEPORT = os.getenv("FREEPORT", 9999)
+
+
+class SimpleTestServerKeyMapTest(unittest.TestCase):
+    def __init__(self, *argv, **kwargs):
+        super(SimpleTestServerKeyMapTest, self).__init__(*argv, **kwargs)
+
+        # This is to show the whole difference
+        self.maxDiff = None
+        # This is to fix the difference using the difflib
+        self.addTypeEqualityFunc(str, self.assertMultiLineEqual)
+        self.addTypeEqualityFunc(unicode, self.assertMultiLineEqual)
+
+    def setUp(self):
+        self.SIMPLE_REDIS_CONF = """
+activerehashing yes
+auto-aof-rewrite-percentage 100
+client-output-buffer-limit normal 0 0 0
+client-output-buffer-limit pubsub 32mb 8mb 60
+client-output-buffer-limit slave 256mb 64mb 60
+dbfilename dump.rdb
+dir ./
+hash-max-ziplist-entries 512
+list-max-ziplist-entries 512
+repl-disable-tcp-nodelay no
+save 300 10
+save 60 10000
+save 900 1
+zset-max-ziplist-entries 128
+zset-max-ziplist-value 64
+"""
+        self.conf = redis.test_server.KeyPairMapping(self.SIMPLE_REDIS_CONF)
+
+    def test_reading_into_a_keypairmapping_class(self):
+        "test verifying the correct multivalued keys load"
+
+        result = redis.test_server.KeyPairMapping.to_str(self.conf)
+        self.assertEquals(self.SIMPLE_REDIS_CONF.strip(), result)
+
+    def test_values_in_a_keypairmapping_class(self):
+        "test checking config values loaded"
+
+        result = redis.test_server.KeyPairMapping.to_str(self.conf)
+        self.assertEquals(self.conf['save'], ['300 10', '60 10000', '900 1'])
+        self.assertEquals(self.conf['dir'], './')
+        self.assertEquals(self.conf['zset-max-ziplist-value'], '64')
 
 
 class SimpleTestServerCase(unittest.TestCase):
