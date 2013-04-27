709e7cd2b59d70a8e24fd31b798c02af5602e08d
andy <andy@whiskeymedia.com>
2013-04-27 10:51:51 -0700

    finer grain tests

--- tests/server_commands.py
+++ tests/server_commands.py
@@ -471,13 +471,13 @@ class ServerCommandsTestCase(unittest.TestCase):
     def test_set_px(self):
         self.assertEquals(self.client.set('foo', '1', px=10000), True)
         self.assertEquals(self.client['foo'], b('1'))
-        self.assert_(self.client.pttl('foo'), 10000)
-        self.assert_(self.client.ttl('foo'), 10)
+        self.assert_(0 < self.client.pttl('foo') <= 10000)
+        self.assert_(0 < self.client.ttl('foo') <= 10)
         # expire given a timedelta
         expire_at = datetime.timedelta(milliseconds=1000)
         self.assertEquals(self.client.set('foo', '1', px=expire_at), True)
-        self.assert_(self.client.pttl('foo'), 1000)
-        self.assert_(self.client.ttl('foo'), 1)
+        self.assert_(0 < self.client.pttl('foo') <= 1000)
+        self.assert_(0 < self.client.ttl('foo') <= 1)
 
     def test_set_ex(self):
         self.assertEquals(self.client.set('foo', '1', ex=10), True)
@@ -489,7 +489,9 @@ class ServerCommandsTestCase(unittest.TestCase):
 
     def test_set_multipleoptions(self):
         self.client['foo'] = 'val'
-        self.assertEquals(self.client.set('foo', 'bar', xx=True, px=10000), True)
+        self.assertEquals(
+            self.client.set('foo', 'bar', xx=True, px=10000),
+            True)
         self.assertEquals(self.client.ttl('foo'), 10)
 
     def test_setex(self):
