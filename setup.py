d93a7fa3902a107cb09e2ccc11c591103e101634
andy <andy@whiskeymedia.com>
2013-04-28 10:23:32 -0700

    use README.rst for long description

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ try:
 except ImportError:
     from distutils.core import setup
 
-f = open(os.path.join(os.path.dirname(__file__), 'README.md'))
+f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
 long_description = f.read()
 f.close()
 
