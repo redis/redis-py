163e9b7fb9d0b99ff63b02a7f36433fac82d9e5e
Andy McCurdy <andy@andymccurdy.com>
2014-01-28 12:01:06 -0800

    with tox/travis, tests don't need to know about the environment

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -2,16 +2,14 @@ from __future__ import with_statement
 import pytest
 
 from redis._compat import unichr, u, unicode
-from redis.utils import HIREDIS_AVAILABLE
 from .conftest import r as _redis_client
 
 
-@pytest.fixture()
-def r(request):
-    return _redis_client(request=request, decode_responses=True)
+class TestEncoding(object):
+    @pytest.fixture()
+    def r(self, request):
+        return _redis_client(request=request, decode_responses=True)
 
-
-class BaseEncodingTests(object):
     def test_simple_encoding(self, r):
         unicode_string = unichr(3456) + u('abcd') + unichr(3421)
         r['unicode-string'] = unicode_string
@@ -24,11 +22,3 @@ class BaseEncodingTests(object):
         result = [unicode_string, unicode_string, unicode_string]
         r.rpush('a', *result)
         assert r.lrange('a', 0, -1) == result
-
-
-class TestPythonParserEncoding(BaseEncodingTests):
-    pass
-
-if HIREDIS_AVAILABLE:
-    class TestHiredisParserEncoding(BaseEncodingTests):
-        pass
