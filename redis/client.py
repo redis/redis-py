2b86eaaa7b07a1e29f39e197798be8c4524bf230
Tin [Tian Yue] <iamtin@gmail.com>
2014-02-24 16:21:11 -0800

    Add block_timeout to StrictRedis.lock. So when we lock a resource exlusively (timeout is long, or forever), we won't let others wait forever (stop blocking on a threshold).

--- redis/client.py
+++ redis/client.py
@@ -428,7 +428,7 @@ class StrictRedis(object):
                 except WatchError:
                     continue
 
-    def lock(self, name, timeout=None, sleep=0.1):
+    def lock(self, name, timeout=None, sleep=0.1, blocking_timeout=None):
         """
         Return a new Lock object using key ``name`` that mimics
         the behavior of threading.Lock.
@@ -440,7 +440,7 @@ class StrictRedis(object):
         when the lock is in blocking mode and another client is currently
         holding the lock.
         """
-        return Lock(self, name, timeout=timeout, sleep=sleep)
+        return Lock(self, name, timeout=timeout, sleep=sleep, blocking_timeout=blocking_timeout)
 
     def pubsub(self, shard_hint=None):
         """
@@ -2236,13 +2236,18 @@ class Lock(object):
 
     LOCK_FOREVER = float(2 ** 31 + 1)  # 1 past max unix time
 
-    def __init__(self, redis, name, timeout=None, sleep=0.1):
+    def __init__(self, redis, name, timeout=None, sleep=0.1, blocking_timeout=None):
         """
         Create a new Lock instnace named ``name`` using the Redis client
         supplied by ``redis``.
 
         ``timeout`` indicates a maximum life for the lock.
         By default, it will remain locked until release() is called.
+
+
+        ``blocking_timeout`` indicates block wait threshold.
+        So the code won't wait a lock longer than this threshold when current lock
+        holder need hold this lock to finish a time consuming task.
 
         ``sleep`` indicates the amount of time to sleep per loop iteration
         when the lock is in blocking mode and another client is currently
@@ -2256,6 +2261,7 @@ class Lock(object):
         self.name = name
         self.acquired_until = None
         self.timeout = timeout
+        self.blocking_timeout = blocking_timeout
         self.sleep = sleep
         if self.timeout and self.sleep > self.timeout:
             raise LockError("'sleep' must be less than 'timeout'")
@@ -2276,6 +2282,11 @@ class Lock(object):
         """
         sleep = self.sleep
         timeout = self.timeout
+        if self.blocking_timeout:
+            block_until = mod_time.time() + self.blocking_timeout
+        else:
+            block_until = Lock.LOCK_FOREVER
+
         while 1:
             unixtime = mod_time.time()
             if timeout:
@@ -2296,8 +2307,9 @@ class Lock(object):
                     # we successfully acquired the lock
                     self.acquired_until = timeout_at
                     return True
-            if not blocking:
+            if (not blocking) or (unixtime > block_until):
                 return False
+
             mod_time.sleep(sleep)
 
     def release(self):
