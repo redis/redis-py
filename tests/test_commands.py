be75147581afe01796e8e2add6925a3e8488cfbb
Andy McCurdy <andy@andymccurdy.com>
2014-04-08 19:22:06 -0700

    lame, typo

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -99,7 +99,7 @@ class TestRedisCommands(object):
         # fail on purpose on travis
         assert [
             r.config_get('slowlog-log-slower-than'),
-            r.config.get('slowlog-max-len')] == [-10, -10]
+            r.config_get('slowlog-max-len')] == [-10, -10]
         assert r.slowlog_reset()
         unicode_string = unichr(3456) + u('abcd') + unichr(3421)
         r.get(unicode_string)
