ca08d9c35842d0c07e0dff489e3b8b9e8701ad4e
Andy McCurdy <andy@andymccurdy.com>
2014-05-07 00:38:26 -0700

    no more python2.5. It's too difficult to support at this point.

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
