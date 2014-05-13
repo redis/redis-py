038b3ca240e5744ac8f8f9d6d05df90c95097b7e
Andy McCurdy <andy@andymccurdy.com>
2014-05-12 17:52:27 -0700

    add SSLConnection to package

--- redis/__init__.py
+++ redis/__init__.py
@@ -3,6 +3,7 @@ from redis.connection import (
     BlockingConnectionPool,
     ConnectionPool,
     Connection,
+    SSLConnection,
     UnixDomainSocketConnection
 )
 from redis.utils import from_url
@@ -25,7 +26,7 @@ VERSION = tuple(map(int, __version__.split('.')))
 
 __all__ = [
     'Redis', 'StrictRedis', 'ConnectionPool', 'BlockingConnectionPool',
-    'Connection', 'UnixDomainSocketConnection',
+    'Connection', 'SSLConnection', 'UnixDomainSocketConnection',
     'RedisError', 'ConnectionError', 'ResponseError', 'AuthenticationError',
     'InvalidResponse', 'DataError', 'PubSubError', 'WatchError', 'from_url',
     'BusyLoadingError', 'ReadOnlyError'
