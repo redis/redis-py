8b4959cb1ab773ab4526e643970557bc13224609
Corey Farwell <coreyf@rwell.org>
2014-05-27 14:27:34 -0700

    Add Python 3.4 setuptools classifier

--- setup.py
+++ setup.py
@@ -56,5 +56,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ]
 )
