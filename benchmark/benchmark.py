d8745811e7ceb05edc13f95ce2e8bcba06afad35
黄毅 <yi.codeplayer@gmail.com>
2014-10-28 15:19:14 +0800

    pass py3 tests

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -4,14 +4,14 @@ sys.path.insert(0, '..')
 import patch_socket
 
 import redis
-print redis.__file__
+print(redis.__file__)
 
 try:
     import redis._pack
 except ImportError:
-    print 'not pure python _pack_command'
+    print('not pure python _pack_command')
 else:
-    print 'using c _pack_command'
+    print('using c _pack_command')
 from redis.connection import Connection
 
 
@@ -58,5 +58,7 @@ timeit.main(['-s', 'from __main__ import run_with_replay, pool, bench',
             '-n', '10000', 'run_with_replay(pool.conn._sock, bench)'])
 
 import cProfile
+if sys.version_info[0] >= 3:
+    xrange = range
 cProfile.run('for i in xrange(10000):run_with_replay(pool.conn._sock, bench)',
              sort='time')
--- redis/_pack.pyx
+++ redis/_pack.pyx
@@ -8,16 +8,16 @@ cdef bytes SYM_LF = b'\n'
 cdef extern from "Python.h":
     object PyObject_Str(object v)
 
-cdef _encode(self, value):
+cdef bytes _encode(self, value):
     "Return a bytestring representation of the value"
     if isinstance(value, bytes):
         return value
     elif isinstance(value, float):
-        return bytes(repr(value))
+        return bytes(repr(value), 'latin-1')
     elif not isinstance(value, basestring):
         value = PyObject_Str(value)
     elif isinstance(value, Token):
-        return bytes(value.value)
+        return bytes(value.value, 'latin-1')
 
     if isinstance(value, unicode):
         value = (<unicode>value).encode(self.encoding, self.encoding_errors)
@@ -33,12 +33,12 @@ def _pack_command(self, *args):
     else:
         args = (Token(command),) + args[1:]
 
-    output = [SYM_STAR, PyObject_Str(len(args)), SYM_CRLF]
+    output = [SYM_STAR, bytes(PyObject_Str(len(args)), 'latin-1'), SYM_CRLF]
     for value in args:
         enc_value = _encode(self, value)
         output.append(SYM_DOLLAR)
-        output.append(PyObject_Str(len(enc_value)))
+        output.append(bytes(PyObject_Str(len(enc_value)), 'latin-1'))
         output.append(SYM_CRLF)
         output.append(enc_value)
         output.append(SYM_CRLF)
-    return b''.join(output)
+    return output
