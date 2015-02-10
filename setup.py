92e3618d34549a652ea6c67e07c617f2e2fd848a
Paul Keene <paulkeene4@gmail.com>
2015-02-10 10:22:00 -0800

    Fix pep8 errors

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ try:
 except ImportError:
 
     from distutils.core import setup
-    PyTest = lambda x: x
+
+    def PyTest(x): x
 
 f = open(os.path.join(os.path.dirname(__file__), 'README.rst'))
 long_description = f.read()
