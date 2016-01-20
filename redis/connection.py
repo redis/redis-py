620e912e9f4ca0532c8967f0f2ce95020e083816
Baran Bartu Demirci <baranbartu@Barans-MacBook-Air.local>
2016-01-20 14:25:13 +0200

    fix test bugs, passed all tests

--- redis/connection.py
+++ redis/connection.py
@@ -96,16 +96,10 @@ class BaseParser(object):
             response = response[len(error_code) + 1:]
             exception_class = self.EXCEPTION_CLASSES[error_code]
             if isinstance(exception_class, dict):
-                found = False
                 for reason, inner_exception_class in exception_class.items():
-                    new_response = response.replace(reason, '').strip()
-                    if new_response != response:
-                        exception_class = inner_exception_class
-                        response = new_response
-                        found = True
-                        break
-                if not found:
-                    exception_class = ResponseError
+                    if reason in response:
+                        return inner_exception_class(response)
+                return ResponseError(response)
             return exception_class(response)
         return ResponseError(response)
 
