2eef463380570aab5a32b2355a1323dee4613294
Vamsi Atluri <vamc19@gmail.com>
2016-12-11 23:54:54 +0530

    Support for loading, unloading and listing Redis Modules

--- redis/exceptions.py
+++ redis/exceptions.py
@@ -57,6 +57,10 @@ class NoPermissionError(ResponseError):
     pass
 
 
+class ModuleError(RedisError):
+    pass
+
+
 class LockError(RedisError, ValueError):
     "Errors acquiring or releasing a lock"
     # NOTE: For backwards compatability, this class derives from ValueError.
