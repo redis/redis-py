ffccbbf8424d868b11f7cd9e59649ab0f67a0391
Raphael Deem <raphael.deem@gmail.com>
2016-12-22 13:18:42 -0800

    test zcount exclusive range

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -901,6 +901,8 @@ class TestRedisCommands(object):
         r.zadd('a', a1=1, a2=2, a3=3)
         assert r.zcount('a', '-inf', '+inf') == 3
         assert r.zcount('a', 1, 2) == 2
+        assert r.zcount('a', '(' + str(1), 2) == 1
+        assert r.zcount('a', 1, '(' + str(2)) == 1
         assert r.zcount('a', 10, 20) == 0
 
     def test_zincrby(self, r):
