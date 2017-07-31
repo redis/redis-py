037771123901539089a1f8f72940622539b8f9c1
Andy McCurdy <andy@andymccurdy.com>
2017-07-31 12:46:43 -0400

    skip HSTRLEN tests if server < 3.2.0

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1229,6 +1229,7 @@ class TestRedisCommands(object):
         remote_vals = r.hvals('a')
         assert sorted(local_vals) == sorted(remote_vals)
 
+    @skip_if_server_version_lt('3.2.0')
     def test_hstrlen(self, r):
         r.hmset('a', {'1': '22', '2': '333'})
         assert r.hstrlen('a', '1') == 2
