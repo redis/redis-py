ba79979a46c63355b81b24e769200e5b65507b84
Jon Dufresne <jon.dufresne@gmail.com>
2018-01-05 05:47:36 -0800

    Pass python_requires argument to setuptools
    Helps pip decide what version of the library to install.
    
    https://packaging.python.org/tutorials/distributing-packages/#python-requires
    
    > If your project only runs on certain Python versions, setting the
    > python_requires argument to the appropriate PEP 440 version specifier
    > string will prevent pip from installing the project on other Python
    > versions.
    
    https://setuptools.readthedocs.io/en/latest/setuptools.html#new-and-changed-setup-keywords
    
    > python_requires
    >
    > A string corresponding to a version specifier (as defined in PEP 440)
    > for the Python version, used to specify the Requires-Python defined in
    > PEP 345.

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
     keywords=['Redis', 'key-value store'],
     license='MIT',
     packages=['redis'],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     extras_require={
         'hiredis': [
             "hiredis>=0.1.3",
