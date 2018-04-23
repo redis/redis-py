39e50d647e1b5ba7192f018543ae4c943070ecd2
jdupl123 <jaco.duplessis@uqconnect.edu.au>
2018-04-23 14:48:04 +1000

    Update connection.py
    test string rather than parse object

--- redis/connection.py
+++ redis/connection.py
@@ -835,8 +835,8 @@ class ConnectionPool(object):
         qs = ''
 
         schemes = ['redis://', 'rediss://', 'unix://']
-        if not any([url.startswith(scheme) for scheme in schemes]):
-            raise ValueError("url {} must start with one of {}".format(url, schemes))
+        if not any([url_string.startswith(scheme) for scheme in schemes]):
+            raise ValueError("url {} must start with one of {}".format(url_string, schemes))
 
         # in python2.6, custom URL schemes don't recognize querystring values
         # they're left as part of the url.path.
