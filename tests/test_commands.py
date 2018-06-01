d983c3f80fb2f116eb23cbf99ce1ecbd1b0e211d
rufo <rufus@rufus.al>
2018-06-02 00:48:45 +0100

    Add ZPOPMIN and ZPOPMAX commands and corresponding tests

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1139,6 +1139,22 @@ class TestRedisCommands(object):
         assert r.zrange('d', 0, -1, withscores=True) == \
             [(b('a2'), 5), (b('a4'), 12), (b('a3'), 20), (b('a1'), 23)]
 
+    @skip_if_server_version_lt('5.0.0')
+    def test_zpopmax(self, r):
+        r.zadd('a', a=4, b=3, c=2, d=1)
+        assert r.zpopmax('a') == [(b('a'), 4)]
+        assert r.zpopmax('a', 2) == [(b('b'), 3), (b('c'), 2)]
+        assert r.zpopmax('a', 2) == [(b('d'), 1)]
+        assert r.zpopmax('a') == []
+
+    @skip_if_server_version_lt('5.0.0')
+    def test_zpopmin(self, r):
+        r.zadd('a', a=1, b=2, c=3, d=4)
+        assert r.zpopmin('a') == [(b('a'), 1)]
+        assert r.zpopmin('a', 2) == [(b('b'), 2), (b('c'), 3)]
+        assert r.zpopmin('a', 2) == [(b('d'), 4)]
+        assert r.zpopmin('a') == []
+
     # HYPERLOGLOG TESTS
     @skip_if_server_version_lt('2.8.9')
     def test_pfadd(self, r):
