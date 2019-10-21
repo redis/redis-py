8aea4bea1876e647aabf428712dd0a74a2092092
Jamie Scott <jamie.scott@uconn.edu>
2019-10-21 03:48:35 -0700

    Revert "Revert "Whitespace go away""
    This reverts commit 2e65685a7f206f3e9c0b0bf9c808ef85c8817127.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -178,9 +178,9 @@ class TestRedisCommands(object):
         assert r.acl_setuser(username, enabled=True,
                              remove_passwords=[HP])
         assert len(r.acl_getuser(username)['passwords']) == 1
-        
-    @skip_if_server_version_lt('6.0.0')
-    def test_acl_list(self, r, request):
+
+        @skip_if_server_version_lt('6.0.0')
+        def test_acl_list(self, r, request):
         username = 'redis-py-user'
 
         def teardown():
