0ee443ea5d4bb9694db9e3ad3909893614d581ca
Colas Le Guernic <clslgrnc@users.noreply.github.com>
2020-01-23 22:22:13 +0100

    Update test_lock.py

--- tests/test_lock.py
+++ tests/test_lock.py
@@ -91,10 +91,12 @@ class TestLock(object):
     def test_blocking_timeout(self, r):
         lock1 = self.get_lock(r, 'foo')
         assert lock1.acquire(blocking=False)
-        lock2 = self.get_lock(r, 'foo', blocking_timeout=0.2)
+        sleep = 0.05
+        lock2 = self.get_lock(r, 'foo', sleep=sleep, blocking_timeout=0.2)
         start = time.time()
         assert not lock2.acquire()
-        assert (time.time() - start) > 0.2
+        # If we slept, last try would be after blocking_timeout
+        assert (time.time() - start) > 0.2 - sleep
         lock1.release()
 
     def test_context_manager(self, r):
@@ -110,10 +112,15 @@ class TestLock(object):
             with self.get_lock(r, 'foo', blocking_timeout=0.1):
                 pass
 
-    def test_high_sleep_raises_error(self, r):
-        "If sleep is higher than timeout, it should raise an error"
-        with pytest.raises(LockError):
-            self.get_lock(r, 'foo', timeout=1, sleep=2)
+    def test_high_sleep_small_blocking_timeout(self, r):
+        lock1 = self.get_lock(r, 'foo')
+        assert lock1.acquire(blocking=False)
+        sleep = 60
+        lock2 = self.get_lock(r, 'foo', sleep=sleep, blocking_timeout=1)
+        start = time.time()
+        assert not lock2.acquire()
+        assert (time.time() - start) < 1
+        lock1.release()
 
     def test_releasing_unlocked_lock_raises_error(self, r):
         lock = self.get_lock(r, 'foo')
