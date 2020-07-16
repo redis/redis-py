07dd8e332f39eefe04543c9cad6e2c70a6eec981
Andy McCurdy <andy@andymccurdy.com>
2020-07-16 15:49:52 -0700

    debug

--- docker-entry.sh
+++ docker-entry.sh
@@ -8,6 +8,9 @@ set -eu
 REDIS_MASTER="${REDIS_MASTER_HOST}":"${REDIS_MASTER_PORT}"
 echo "Testing against Redis Server: ${REDIS_MASTER}"
 
+#debug
+echo `bash <(curl -s https://codecov.io/env)`
+
 # use the wait-for-it util to ensure the server is running before invoking Tox
 util/wait-for-it.sh ${REDIS_MASTER} -- tox -- --redis-url=redis://"${REDIS_MASTER}"/9
 
