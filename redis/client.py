a3f2826f6561fbb6e0a9366c6a2efac9d4aa6838
Chayim <chayim@users.noreply.github.com>
2021-07-25 09:04:15 +0300

    support for client unpause (#1512)

--- redis/client.py
+++ redis/client.py
@@ -1305,6 +1305,12 @@ class Redis:
             raise DataError("CLIENT PAUSE timeout must be an integer")
         return self.execute_command('CLIENT PAUSE', str(timeout))
 
+    def client_unpause(self):
+        """
+        Unpause all redis clients
+        """
+        return self.execute_command('CLIENT UNPAUSE')
+
     def readwrite(self):
         "Disables read queries for a connection to a Redis Cluster slave node"
         return self.execute_command('READWRITE')
