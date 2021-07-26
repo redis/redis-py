28a4cf209a41481db170a61dcb1ee5d6d1ab6493
AvitalFineRedis <avital.fine@redislabs.com>
2021-07-26 15:05:20 +0300

    add documentations to the functions

--- redis/client.py
+++ redis/client.py
@@ -3466,6 +3466,50 @@ class Redis:
                   unit='m', radius=None, width=None, height=None, sort=None,
                   count=None, any=False, withcoord=False,
                   withdist=False, withhash=False):
+        """
+        Return the members of specified key identified by the
+        ``name`` argument, which are within the borders of the
+        area specified by a given shape. This command extends the
+        GEORADIUS command, so in addition to searching within circular
+        areas, it supports searching within rectangular areas.
+
+        This command should be used in place of the deprecated
+        GEORADIUS and GEORADIUSBYMEMBER commands.
+
+        ``member`` Use the position of the given existing
+         member in the sorted set. Can't be given with ``longitude``
+         and ``latitude``.
+
+        ``longitude`` and ``latitude`` Use the position given by
+        this coordinates. Can't be given with ``member``
+
+        ``radius`` Similar to GEORADIUS, search inside circular
+        area according the given radius. Can't be given with
+        ``height`` and ``width``.
+
+        ``height`` and ``width`` Search inside an axis-aligned
+        rectangle, determined by the given height and width.
+        Can't be given with ``radius``
+
+        ``unit`` must be one of the following : m, km, mi, ft.
+        `m` for meters (the default value), `km` for kilometers,
+        `mi` for miles and `ft` for feet.
+
+        ``sort`` indicates to return the places in a sorted way,
+        ASC for nearest to farest and DESC for farest to nearest.
+
+        ``count`` limit the results to the first count matching items.
+
+        ``any`` is set to True, the command will return as soon as
+        enough matches are found. Can't be provided without ``count``
+
+        ``withdist`` indicates to return the distances of each place.
+
+        ``withcoord`` indicates to return the latitude and longitude of
+        each place.
+
+        ``withhash`` indicates to return the geohash string of each place.
+        """
 
         return self._geosearchgeneric('GEOSEARCH',
                                       name, member=member, longitude=longitude,
@@ -3480,7 +3524,15 @@ class Redis:
                        latitude=None, unit='m', radius=None, width=None,
                        height=None, sort=None, count=None, any=False,
                        storedist=False):
+        """
+        This command is like GEOSEARCH, but stores the result in
+        ``dest``. By default, it stores the results in the destination
+        sorted set with their geospatial information.
 
+        if ``store_dist`` set to True, the command will stores the
+        items in a sorted set populated with their distance from the
+        center of the circle or box, as a floating-point number.
+        """
         return self._geosearchgeneric('GEOSEARCHSTORE',
                                       dest, name, member=member,
                                       longitude=longitude, latitude=latitude,
@@ -3492,6 +3544,7 @@ class Redis:
 
     def _geosearchgeneric(self, command, *args, **kwargs):
         pieces = list(args)
+
         # FROMMEMBER or FROMLONLAT
         if kwargs['member'] is None:
             if kwargs['longitude'] is None or kwargs['latitude'] is None:
@@ -3524,7 +3577,7 @@ class Redis:
             pieces.extend([b'BYBOX',
                            kwargs['width'], kwargs['height'], kwargs['unit']])
 
-        # ASC and DESC
+        # sort
         if kwargs['sort']:
             if kwargs['sort'].upper() == 'ASC':
                 pieces.append(b'ASC')
@@ -3541,7 +3594,7 @@ class Redis:
         elif kwargs['any']:
             raise DataError("GEOSEARCH any can't be provided without count")
 
-        # withcoord, withdist and withhash
+        # other properties
         for arg_name, byte_repr in (
                 ('withdist', b'WITHDIST'),
                 ('withcoord', b'WITHCOORD'),
@@ -3556,7 +3609,7 @@ class Redis:
                   withdist=False, withcoord=False, withhash=False, count=None,
                   sort=None, store=None, store_dist=None):
         """
-        Return the members of the specified key identified by the
+        Return the members of specified key identified by the
         ``name`` argument which are within the borders of the area specified
         with the ``latitude`` and ``longitude`` location and the maximum
         distance from the center specified by the ``radius`` value.
