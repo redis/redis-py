853ff11b1a354adfce990ee75e1448a8626e0e90
AvitalFineRedis <avital.fine@redislabs.com>
2021-07-26 14:33:40 +0300

    change georadius_generic to geosearch_generic

--- redis/client.py
+++ redis/client.py
@@ -441,7 +441,7 @@ def parse_cluster_nodes(response, **options):
     return dict(_parse_node_line(line) for line in raw_lines)
 
 
-def parse_georadius_generic(response, **options):
+def parse_geosearch_generic(response, **options):
     if options['store'] or options['store_dist']:
         # `store` and `store_diff` cant be combined
         # with other command arguments.
@@ -648,9 +648,9 @@ class Redis:
         'GEOPOS': lambda r: list(map(lambda ll: (float(ll[0]),
                                      float(ll[1]))
                                      if ll is not None else None, r)),
-        'GEOSEARCH': parse_georadius_generic,
-        'GEORADIUS': parse_georadius_generic,
-        'GEORADIUSBYMEMBER': parse_georadius_generic,
+        'GEOSEARCH': parse_geosearch_generic,
+        'GEORADIUS': parse_geosearch_generic,
+        'GEORADIUSBYMEMBER': parse_geosearch_generic,
         'HGETALL': lambda r: r and pairs_to_dict(r) or {},
         'HSCAN': parse_hscan,
         'INFO': parse_info,
