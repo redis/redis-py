18c1cc7482d2177809d381e91721f119117849ff
Chayim <chayim@users.noreply.github.com>
2021-09-01 11:44:36 +0300

    Support for command count (#1554)
    Part of #1546

--- redis/commands.py
+++ redis/commands.py
@@ -2924,6 +2924,9 @@ class Commands:
         """
         return self.execute_command('MODULE LIST')
 
+    def command_count(self):
+        return self.execute_command('COMMAND COUNT')
+
 
 class Script:
     "An executable Lua script object returned by ``register_script``"
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -3240,6 +3240,12 @@ class TestRedisCommands:
         assert isinstance(r.module_list(), list)
         assert not r.module_list()
 
+    @skip_if_server_version_lt('2.8.13')
+    def test_command_count(self, r):
+        res = r.command_count()
+        assert isinstance(res, int)
+        assert res >= 100
+
 
 class TestBinarySave:
 
