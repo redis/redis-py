9da4416ec64272051e89b8f49f29016b77ca1a4f
Chayim I. Kirshen <c@kirshen.com>
2021-10-26 14:01:49 +0300

    fixing for new commands, json + search

--- setup.py
+++ setup.py
@@ -2,38 +2,42 @@
 from setuptools import setup, find_packages
 import redis
 
-#setup()
-setup (
-    name='redis',
-    description='Python client for Redis database and key-value store',
-    long_description=open('README.md').read().strip(),
-    keywords=['Redis', 'key-value store', 'database'],
-    license='MIT',
+setup(
+    name="redis",
+    description="Python client for Redis database and key-value store",
+    long_description=open("README.md").read().strip(),
+    keywords=["Redis", "key-value store", "database"],
+    license="MIT",
     version=redis.__version__,
-    packages=find_packages(include=['redis', 'redis.commands']),
-    url='https://github.com/redis/redis-py',
-    author='Redis Inc.',
-    author_email='oss@redis.com',
-
-    classifiers =[
-    'Development Status :: 5 - Production/Stable',
-    'Environment :: Console',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3 :: Only',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-    'Programming Language :: Python :: Implementation :: CPython',
-    'Programming Language :: Python :: Implementation :: PyPy',
+    packages=find_packages(
+        include=[
+            "redis",
+            "redis.commands",
+            "redis.commands.json",
+            "redis.commands.search",
+        ]
+    ),
+    url="https://github.com/redis/redis-py",
+    author="Redis Inc.",
+    author_email="oss@redis.com",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
-
     extras_require={
-        'hiredis': ['hiredis>=1.0.0'],
-    }
+        "hiredis": ["hiredis>=1.0.0"],
+    },
 )
