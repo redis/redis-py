1983905d5adceaba2c3b27ba8f569dcb5387cc35
Éric Lemoine <eric.lemoine@gmail.com>
2022-02-22 12:32:14 +0100

    Catch OSError in BlockingConnectionPool.get_connection (#2001)
    Co-authored-by: Chayim I. Kirshen <c@kirshen.com>

--- redis/connection.py
+++ redis/connection.py
@@ -1577,7 +1577,7 @@ class BlockingConnectionPool(ConnectionPool):
             try:
                 if connection.can_read():
                     raise ConnectionError("Connection has data")
-            except ConnectionError:
+            except (ConnectionError, OSError):
                 connection.disconnect()
                 connection.connect()
                 if connection.can_read():
