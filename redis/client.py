8919cd8f5f4201f2f5aab0f41ff8f4f4e69cd74d
dvora-h <67596500+dvora-h@users.noreply.github.com>
2022-03-14 11:16:16 +0200

    linters

--- redis/client.py
+++ redis/client.py
@@ -1149,7 +1149,8 @@ class Redis(AbstractRedis, RedisModuleCommands, CoreCommands, SentinelCommands):
         self.close()
 
     def close(self):
-        # In case a connection property does not yet exist (due to a crash earlier in the Redis() constructor), return
+        # In case a connection property does not yet exist
+        # (due to a crash earlier in the Redis() constructor), return
         # immediately as there is nothing to clean-up.
         if not hasattr(self, 'connection'):
             return
