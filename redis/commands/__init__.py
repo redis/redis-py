1f29a42e8c2b8f8b371b6841c84a677e0d8a91aa
dvora-h <dvora.heller@redis.com>
2022-04-07 19:13:49 +0300

    linters

--- redis/commands/__init__.py
+++ redis/commands/__init__.py
@@ -2,7 +2,7 @@ from .cluster import RedisClusterCommands
 from .core import AsyncCoreCommands, CoreCommands
 from .helpers import list_or_args
 from .parser import CommandsParser
-from .redismodules import RedisModuleCommands, AsyncRedisModuleCommands
+from .redismodules import AsyncRedisModuleCommands, RedisModuleCommands
 from .sentinel import AsyncSentinelCommands, SentinelCommands
 
 __all__ = [
