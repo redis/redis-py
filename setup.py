805b1842dbf7a732bad484e418dd7da4ec6ab890
Chayim <chayim@users.noreply.github.com>
2022-04-13 13:41:28 +0300

    Adding (experimental) python 3.11 CI (#1991)

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
