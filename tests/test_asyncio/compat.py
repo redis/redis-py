15e4169b0f1197d776b6521733913becf2cb7664
dvora-h <dvora.heller@redis.com>
2022-07-28 03:50:53 +0300

    Replace `ensure_future` with `create_task`

--- tests/test_asyncio/compat.py
+++ tests/test_asyncio/compat.py
@@ -9,7 +9,4 @@ except AttributeError:
 
 
 def create_task(coroutine):
-    if sys.version_info[:2] >= (3, 7):
-        return asyncio.create_task(coroutine)
-    else:
-        return asyncio.ensure_future(coroutine)
+    return asyncio.create_task(coroutine)
