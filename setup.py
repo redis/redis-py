2200534a7681ee0c53da3dda8f37bdf75643cbea
Chayim I. Kirshen <c@kirshen.com>
2022-12-04 10:07:39 +0200

    Version 4.4.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     long_description_content_type="text/markdown",
     keywords=["Redis", "key-value store", "database"],
     license="MIT",
-    version="4.4.0rc4",
+    version="4.4.0",
     packages=find_packages(
         include=[
             "redis",
