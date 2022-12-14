74c251a19d211b3dcf555adc4a68bdc6f097f763
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2022-12-14 14:41:33 +0200

    Add OpenTelemetry example with Uptrace backend (#2452)
    * chore: add opentelemetry example
    
    * chore: add opentelemetry API Jupyter notebook
    
    * chore: use a shorter title
    
    * chore: cleanup
    
    Co-authored-by: dvora-h <67596500+dvora-h@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -162,7 +162,7 @@ html_logo = "_static/redis-cube-red-white-rgb.svg"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ["_static", "images"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
