3f48b271ee9ce044a4ef115e352c967d488e89e4
aksinha334 <aksinha334@gmail.com>
2023-03-17 20:26:19 +0530

    Added test for current PR

--- tests/test_command_parser.py
+++ tests/test_command_parser.py
@@ -20,6 +20,13 @@ class TestCommandsParser:
         assert commands_parser.get_keys(r, *args2) == ["foo"]
         assert commands_parser.get_keys(r, *args3) == ["foo", "bar", "foobar"]
 
+    def test_step_count_as_zero_not_failing(self, r):
+        commands_parser = CommandsParser(r)
+        # Intentionally updating
+        commands_parser.update({"first_key_pos": 1, "last_key_pos": 0, "step_count": 0})
+        args1 = ["GET", "foo"]
+        assert commands_parser.get_keys(r, *args1) == []
+
     @pytest.mark.filterwarnings("ignore:ResponseError")
     @skip_if_redis_enterprise()
     def test_get_moveable_keys(self, r):
