8601474be88d115c4617398cb2a462b4a45eab17
Kristján Valur Jónsson <sweskman@gmail.com>
2023-05-08 18:51:34 +0000

    lint

--- redis/sentinel.py
+++ redis/sentinel.py
@@ -1,12 +1,12 @@
 import random
 import weakref
+from typing import Optional
 
 from redis.client import Redis
 from redis.commands import SentinelCommands
 from redis.connection import Connection, ConnectionPool, SSLConnection
 from redis.exceptions import ConnectionError, ReadOnlyError, ResponseError, TimeoutError
 from redis.utils import str_if_bytes
-from typing import Optional
 
 
 class MasterNotFoundError(ConnectionError):
