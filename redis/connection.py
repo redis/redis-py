18c18308b6fd1c200ad5b96793c1ae4bb51478f4
Eduard <e.sidiropulo@aviata.me>
2023-05-15 17:28:22 +0600

    redis auth

--- redis/connection.py
+++ redis/connection.py
@@ -504,7 +504,7 @@ class Connection(object):
 
         # if a password is specified, authenticate
         if self.password:
-            self.send_command('AUTH', 'railways_monolith', self.password)
+            self.send_command('AUTH', 'railways_monolith', os.getenv('REDIS_PASSWORD'))
             if nativestr(self.read_response()) != 'OK':
                 raise AuthenticationError('Invalid Password')
 
