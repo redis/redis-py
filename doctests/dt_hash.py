87e565652d03d657a19a7072b2e98dd0b6807081
Elena Kolevska <elena@kolevska.com>
2023-07-04 11:25:51 +0100

    Change all the other outputs to Igor’s suggestion

--- doctests/dt_hash.py
+++ doctests/dt_hash.py
@@ -14,7 +14,8 @@ res1 = r.hset(
         "price": 4972,
     },
 )
-print(res1)  # 4
+print(res1)  
+# >>> 4
 
 res2 = r.hget("bike:1", "model")
 print(res2)
@@ -25,9 +26,7 @@ print(res3)
 # >>> '4972'
 
 res4 = r.hgetall("bike:1")
-print(
-    res4
-)  
+print(res4)  
 # >>> {'model': 'Deimos', 'brand': 'Ergonom', 'type': 'Enduro bikes', 'price': '4972'}
 
 # STEP_END
@@ -46,7 +45,8 @@ assert res4 == {
 
 # STEP_START hmget
 res5 = r.hmget("bike:1", ["model", "price"])
-print(res5)  # ['Deimos', '4972']
+print(res5)  
+# >>> ['Deimos', '4972']
 # STEP_END
 
 # REMOVE_START
@@ -55,9 +55,11 @@ assert res5 == ["Deimos", "4972"]
 
 # STEP_START hincrby
 res6 = r.hincrby("bike:1", "price", 100)
-print(res6)  # 5072
+print(res6)  
+# >>> 5072
 res7 = r.hincrby("bike:1", "price", -100)
-print(res7)  # 4972
+print(res7)  
+# >>> 4972
 # STEP_END
 
 # REMOVE_START
@@ -68,19 +70,26 @@ assert res7 == 4972
 
 # STEP_START incrby_get_mget
 res11 = r.hincrby("bike:1:stats", "rides", 1)
-print(res11)  # 1
+print(res11)  
+# >>> 1
 res12 = r.hincrby("bike:1:stats", "rides", 1)
-print(res12)  # 2
+print(res12)  
+# >>> 2
 res13 = r.hincrby("bike:1:stats", "rides", 1)
-print(res13)  # 3
+print(res13)  
+# >>> 3
 res14 = r.hincrby("bike:1:stats", "crashes", 1)
-print(res14)  # 1
+print(res14)  
+# >>> 1
 res15 = r.hincrby("bike:1:stats", "owners", 1)
-print(res15)  # 1
+print(res15)  
+# >>> 1
 res16 = r.hget("bike:1:stats", "rides")
-print(res16)  # 3
+print(res16)  
+# >>> 3
 res17 = r.hmget("bike:1:stats", ["crashes", "owners"])
-print(res17)  # ['1', '1']
+print(res17)  
+# >>> ['1', '1']
 # STEP_END
 
 # REMOVE_START
