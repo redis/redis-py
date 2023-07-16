d665dbd797bbcf7c2642419d8893903fa3030cc1
dvora-h <67596500+dvora-h@users.noreply.github.com>
2023-07-16 11:41:32 +0300

    Version 5.0.0rc2 (#2843)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     long_description_content_type="text/markdown",
     keywords=["Redis", "key-value store", "database"],
     license="MIT",
-    version="5.0.0rc1",
+    version="5.0.0rc2",
     packages=find_packages(
         include=[
             "redis",
