83998f5fa9f109d2af428c899b28c812e20331b2
dvora-h <dvora.heller@redis.com>
2023-08-06 05:28:11 +0300

    fix linters

--- redis/commands/search/commands.py
+++ redis/commands/search/commands.py
@@ -386,9 +386,8 @@ class SearchCommands:
         return args
 
     def _mk_query_args(
-        self, 
-        query,
-        query_params: Union[Dict[str, Union[str, int, float, bytes]], None]):
+        self, query, query_params: Union[Dict[str, Union[str, int, float, bytes]], None]
+    ):
         args = [self.index_name]
 
         if isinstance(query, str):
