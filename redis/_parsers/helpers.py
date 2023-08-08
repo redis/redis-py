f5e1bc67f54ca39125bc8160025ba85a04b7cd6c
Chayim I. Kirshen <c@kirshen.com>
2023-08-08 13:52:12 +0300

    fixing parsing issue

--- redis/_parsers/helpers.py
+++ redis/_parsers/helpers.py
@@ -629,7 +629,6 @@ def parse_client_info(value):
     "key1=value1 key2=value2 key3=value3"
     """
     client_info = {}
-    infos = value.split(" ")
     for info in str_if_bytes(value).strip().split():
         key, value = info.split("=")
         client_info[key] = value
