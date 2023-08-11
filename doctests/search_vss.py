e680924e5fd134b59f4df6c0fb28abb66b739d22
David W. Dougherty <dwdougherty@gmail.com>
2023-08-11 07:04:54 -0700

    DOC-2554: update import order

--- doctests/search_vss.py
+++ doctests/search_vss.py
@@ -5,12 +5,14 @@ import time
 
 import numpy as np
 import pandas as pd
-import redis
 import requests
+from sentence_transformers import SentenceTransformer
+
+import redis
 from redis.commands.search.field import NumericField, TagField, TextField, VectorField
 from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 from redis.commands.search.query import Query
-from sentence_transformers import SentenceTransformer
+
 # STEP_END
 
 # STEP_START get_data
