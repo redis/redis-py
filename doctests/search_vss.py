d23058a92d901e11851b1f3c4744b5f45dba05d0
David W. Dougherty <dwdougherty@gmail.com>
2023-08-16 11:07:24 -0700

    DOC-2544: one more attempt using Chayims advice

--- doctests/search_vss.py
+++ doctests/search_vss.py
@@ -5,13 +5,17 @@ import time
 
 import numpy as np
 import pandas as pd
-import requests
-from sentence_transformers import SentenceTransformer
-
 import redis
-from redis.commands.search.field import NumericField, TagField, TextField, VectorField
+import requests
+from redis.commands.search.field import (
+    NumericField,
+    TagField,
+    TextField,
+    VectorField,
+)
 from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 from redis.commands.search.query import Query
+from sentence_transformers import SentenceTransformer
 
 # STEP_END
 
