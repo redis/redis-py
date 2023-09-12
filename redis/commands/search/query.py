7a020bd836f8cd3c5906e129a71584a2959ec70a
meiravgri <109056284+meiravgri@users.noreply.github.com>
2023-09-12 13:29:07 +0300

    Moved validating timeout to timeout()
    Raise the exception when the timeout is set instead of when the query is performed

--- redis/commands/search/query.py
+++ redis/commands/search/query.py
@@ -134,7 +134,10 @@ class Query:
 
     def timeout(self, timeout):
         """overrides the timeout parameter of the module"""
-        self._timeout = timeout
+        if isinstance(timeout, int) and timeout >= 0:
+            self._timeout = timeou
+        else:
+            raise AttributeError("TIMEOUT requires a non negative integer.")
         return self
 
     def in_order(self):
@@ -194,10 +197,8 @@ class Query:
             args += self._ids
         if self._slop >= 0:
             args += ["SLOP", self._slop]
-        if isinstance(self._timeout, int) and self._timeout >= 0:
+        if self._timeout is not None:
             args += ["TIMEOUT", self._timeout]
-        else:
-            raise AttributeError("TIMEOUT requires a non negative integer.")
         if self._in_order:
             args.append("INORDER")
         if self._return_fields:
