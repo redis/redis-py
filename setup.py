a71bf97f4946557544a9cb0dd64d5d25b5ba9956
Dulmandakh <dulmandakh@gmail.com>
2023-09-15 18:23:29 +0800

    remove Python 3.7 from classifiers

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
