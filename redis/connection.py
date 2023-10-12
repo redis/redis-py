1077c0085a4e366799199eff7ae3c7015301cbf3
Jarosław Porada <jaroslaw.porada@booksy.com>
2023-10-12 15:21:24 +0200

    Fix ConnectionPool deadlock triggered by gc
    Garbage collector sometimes are invoked in block of code which acquired ConnectionPool._lock.
    This cause deadlock blocking indefinitely whole thread.

--- redis/connection.py
+++ redis/connection.py
@@ -1008,7 +1008,7 @@ class ConnectionPool:
         )
 
     def reset(self) -> None:
-        self._lock = threading.Lock()
+        self._lock = threading.RLock()
         self._created_connections = 0
         self._available_connections = []
         self._in_use_connections = set()
