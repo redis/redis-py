78ba4ad1c002a5915184e8337e683d5e6ff86e3b
dvora-h <67596500+dvora-h@users.noreply.github.com>
2024-01-07 14:15:58 +0200

    Support client side caching with ConnectionPool (#3099)
    * sync
    
    * async
    
    * fixs connection mocks
    
    * fix async connection mock
    
    * fix test_asyncio/test_connection.py::test_single_connection
    
    * add test for cache blacklist and flushdb at the end of each test
    
    * fix review comments

--- tests/test_asyncio/test_cache.py
+++ tests/test_asyncio/test_cache.py
@@ -2,36 +2,38 @@ import time
 
 import pytest
 import redis.asyncio as redis
+from redis._cache import _LocalCache
 from redis.utils import HIREDIS_AVAILABLE
 
 
 @pytest.mark.skipif(HIREDIS_AVAILABLE, reason="PythonParser only")
 async def test_get_from_cache():
-    r = redis.Redis(cache_enable=True, single_connection_client=True, protocol=3)
+    cache = _LocalCache()
+    r = redis.Redis(protocol=3, client_cache=cache)
     r2 = redis.Redis(protocol=3)
     # add key to redis
     await r.set("foo", "bar")
     # get key from redis and save in local cache
     assert await r.get("foo") == b"bar"
     # get key from local cache
-    assert r.client_cache.get(("GET", "foo")) == b"bar"
+    assert cache.get(("GET", "foo")) == b"bar"
     # change key in redis (cause invalidation)
     await r2.set("foo", "barbar")
     # send any command to redis (process invalidation in background)
     await r.ping()
     # the command is not in the local cache anymore
-    assert r.client_cache.get(("GET", "foo")) is None
+    assert cache.get(("GET", "foo")) is None
     # get key from redis
     assert await r.get("foo") == b"barbar"
 
+    await r.flushdb()
     await r.aclose()
 
 
 @pytest.mark.skipif(HIREDIS_AVAILABLE, reason="PythonParser only")
 async def test_cache_max_size():
-    r = redis.Redis(
-        cache_enable=True, cache_max_size=3, single_connection_client=True, protocol=3
-    )
+    cache = _LocalCache(max_size=3)
+    r = redis.Redis(client_cache=cache, protocol=3)
     # add 3 keys to redis
     await r.set("foo", "bar")
     await r.set("foo2", "bar2")
@@ -41,46 +43,42 @@ async def test_cache_max_size():
     assert await r.get("foo2") == b"bar2"
     assert await r.get("foo3") == b"bar3"
     # get the 3 keys from local cache
-    assert r.client_cache.get(("GET", "foo")) == b"bar"
-    assert r.client_cache.get(("GET", "foo2")) == b"bar2"
-    assert r.client_cache.get(("GET", "foo3")) == b"bar3"
+    assert cache.get(("GET", "foo")) == b"bar"
+    assert cache.get(("GET", "foo2")) == b"bar2"
+    assert cache.get(("GET", "foo3")) == b"bar3"
     # add 1 more key to redis (exceed the max size)
     await r.set("foo4", "bar4")
     assert await r.get("foo4") == b"bar4"
     # the first key is not in the local cache anymore
-    assert r.client_cache.get(("GET", "foo")) is None
+    assert cache.get(("GET", "foo")) is None
 
+    await r.flushdb()
     await r.aclose()
 
 
 @pytest.mark.skipif(HIREDIS_AVAILABLE, reason="PythonParser only")
 async def test_cache_ttl():
-    r = redis.Redis(
-        cache_enable=True, cache_ttl=1, single_connection_client=True, protocol=3
-    )
+    cache = _LocalCache(ttl=1)
+    r = redis.Redis(client_cache=cache, protocol=3)
     # add key to redis
     await r.set("foo", "bar")
     # get key from redis and save in local cache
     assert await r.get("foo") == b"bar"
     # get key from local cache
-    assert r.client_cache.get(("GET", "foo")) == b"bar"
+    assert cache.get(("GET", "foo")) == b"bar"
     # wait for the key to expire
     time.sleep(1)
     # the key is not in the local cache anymore
-    assert r.client_cache.get(("GET", "foo")) is None
+    assert cache.get(("GET", "foo")) is None
 
+    await r.flushdb()
     await r.aclose()
 
 
 @pytest.mark.skipif(HIREDIS_AVAILABLE, reason="PythonParser only")
 async def test_cache_lfu_eviction():
-    r = redis.Redis(
-        cache_enable=True,
-        cache_max_size=3,
-        cache_eviction_policy="lfu",
-        single_connection_client=True,
-        protocol=3,
-    )
+    cache = _LocalCache(max_size=3, eviction_policy="lfu")
+    r = redis.Redis(client_cache=cache, protocol=3)
     # add 3 keys to redis
     await r.set("foo", "bar")
     await r.set("foo2", "bar2")
@@ -90,40 +88,53 @@ async def test_cache_lfu_eviction():
     assert await r.get("foo2") == b"bar2"
     assert await r.get("foo3") == b"bar3"
     # change the order of the keys in the cache
-    assert r.client_cache.get(("GET", "foo")) == b"bar"
-    assert r.client_cache.get(("GET", "foo")) == b"bar"
-    assert r.client_cache.get(("GET", "foo3")) == b"bar3"
+    assert cache.get(("GET", "foo")) == b"bar"
+    assert cache.get(("GET", "foo")) == b"bar"
+    assert cache.get(("GET", "foo3")) == b"bar3"
     # add 1 more key to redis (exceed the max size)
     await r.set("foo4", "bar4")
     assert await r.get("foo4") == b"bar4"
     # test the eviction policy
-    assert len(r.client_cache.cache) == 3
-    assert r.client_cache.get(("GET", "foo")) == b"bar"
-    assert r.client_cache.get(("GET", "foo2")) is None
+    assert len(cache.cache) == 3
+    assert cache.get(("GET", "foo")) == b"bar"
+    assert cache.get(("GET", "foo2")) is None
 
+    await r.flushdb()
     await r.aclose()
 
 
 @pytest.mark.skipif(HIREDIS_AVAILABLE, reason="PythonParser only")
 async def test_cache_decode_response():
-    r = redis.Redis(
-        decode_responses=True,
-        cache_enable=True,
-        single_connection_client=True,
-        protocol=3,
-    )
+    cache = _LocalCache()
+    r = redis.Redis(decode_responses=True, client_cache=cache, protocol=3)
     await r.set("foo", "bar")
     # get key from redis and save in local cache
     assert await r.get("foo") == "bar"
     # get key from local cache
-    assert r.client_cache.get(("GET", "foo")) == "bar"
+    assert cache.get(("GET", "foo")) == "bar"
     # change key in redis (cause invalidation)
     await r.set("foo", "barbar")
     # send any command to redis (process invalidation in background)
     await r.ping()
     # the command is not in the local cache anymore
-    assert r.client_cache.get(("GET", "foo")) is None
+    assert cache.get(("GET", "foo")) is None
     # get key from redis
     assert await r.get("foo") == "barbar"
 
+    await r.flushdb()
+    await r.aclose()
+
+
+@pytest.mark.skipif(HIREDIS_AVAILABLE, reason="PythonParser only")
+async def test_cache_blacklist():
+    cache = _LocalCache()
+    r = redis.Redis(client_cache=cache, cache_blacklist=["LLEN"], protocol=3)
+    # add list to redis
+    await r.lpush("mylist", "foo", "bar", "baz")
+    assert await r.llen("mylist") == 3
+    assert await r.lindex("mylist", 1) == b"bar"
+    assert cache.get(("LLEN", "mylist")) is None
+    assert cache.get(("LINDEX", "mylist", 1)) == b"bar"
+
+    await r.flushdb()
     await r.aclose()
--- tests/test_cache.py
+++ tests/test_cache.py
@@ -2,34 +2,37 @@ import time
 
 import pytest
 import redis
+from redis._cache import _LocalCache
 from redis.utils import HIREDIS_AVAILABLE
 
 
 @pytest.mark.skipif(HIREDIS_AVAILABLE, reason="PythonParser only")
 def test_get_from_cache():
-    r = redis.Redis(cache_enable=True, single_connection_client=True, protocol=3)
+    cache = _LocalCache()
+    r = redis.Redis(protocol=3, client_cache=cache)
     r2 = redis.Redis(protocol=3)
     # add key to redis
     r.set("foo", "bar")
     # get key from redis and save in local cache
     assert r.get("foo") == b"bar"
     # get key from local cache
-    assert r.client_cache.get(("GET", "foo")) == b"bar"
+    assert cache.get(("GET", "foo")) == b"bar"
     # change key in redis (cause invalidation)
     r2.set("foo", "barbar")
     # send any command to redis (process invalidation in background)
     r.ping()
     # the command is not in the local cache anymore
-    assert r.client_cache.get(("GET", "foo")) is None
+    assert cache.get(("GET", "foo")) is None
     # get key from redis
     assert r.get("foo") == b"barbar"
+
+    r.flushdb()
 
 
 @pytest.mark.skipif(HIREDIS_AVAILABLE, reason="PythonParser only")
 def test_cache_max_size():
-    r = redis.Redis(
-        cache_enable=True, cache_max_size=3, single_connection_client=True, protocol=3
-    )
+    cache = _LocalCache(max_size=3)
+    r = redis.Redis(client_cache=cache, protocol=3)
     # add 3 keys to redis
     r.set("foo", "bar")
     r.set("foo2", "bar2")
@@ -39,42 +42,40 @@ def test_cache_max_size():
     assert r.get("foo2") == b"bar2"
     assert r.get("foo3") == b"bar3"
     # get the 3 keys from local cache
-    assert r.client_cache.get(("GET", "foo")) == b"bar"
-    assert r.client_cache.get(("GET", "foo2")) == b"bar2"
-    assert r.client_cache.get(("GET", "foo3")) == b"bar3"
+    assert cache.get(("GET", "foo")) == b"bar"
+    assert cache.get(("GET", "foo2")) == b"bar2"
+    assert cache.get(("GET", "foo3")) == b"bar3"
     # add 1 more key to redis (exceed the max size)
     r.set("foo4", "bar4")
     assert r.get("foo4") == b"bar4"
     # the first key is not in the local cache anymore
-    assert r.client_cache.get(("GET", "foo")) is None
+    assert cache.get(("GET", "foo")) is None
+
+    r.flushdb()
 
 
 @pytest.mark.skipif(HIREDIS_AVAILABLE, reason="PythonParser only")
 def test_cache_ttl():
-    r = redis.Redis(
-        cache_enable=True, cache_ttl=1, single_connection_client=True, protocol=3
-    )
+    cache = _LocalCache(ttl=1)
+    r = redis.Redis(client_cache=cache, protocol=3)
     # add key to redis
     r.set("foo", "bar")
     # get key from redis and save in local cache
     assert r.get("foo") == b"bar"
     # get key from local cache
-    assert r.client_cache.get(("GET", "foo")) == b"bar"
+    assert cache.get(("GET", "foo")) == b"bar"
     # wait for the key to expire
     time.sleep(1)
     # the key is not in the local cache anymore
-    assert r.client_cache.get(("GET", "foo")) is None
+    assert cache.get(("GET", "foo")) is None
+
+    r.flushdb()
 
 
 @pytest.mark.skipif(HIREDIS_AVAILABLE, reason="PythonParser only")
 def test_cache_lfu_eviction():
-    r = redis.Redis(
-        cache_enable=True,
-        cache_max_size=3,
-        cache_eviction_policy="lfu",
-        single_connection_client=True,
-        protocol=3,
-    )
+    cache = _LocalCache(max_size=3, eviction_policy="lfu")
+    r = redis.Redis(client_cache=cache, protocol=3)
     # add 3 keys to redis
     r.set("foo", "bar")
     r.set("foo2", "bar2")
@@ -84,36 +85,50 @@ def test_cache_lfu_eviction():
     assert r.get("foo2") == b"bar2"
     assert r.get("foo3") == b"bar3"
     # change the order of the keys in the cache
-    assert r.client_cache.get(("GET", "foo")) == b"bar"
-    assert r.client_cache.get(("GET", "foo")) == b"bar"
-    assert r.client_cache.get(("GET", "foo3")) == b"bar3"
+    assert cache.get(("GET", "foo")) == b"bar"
+    assert cache.get(("GET", "foo")) == b"bar"
+    assert cache.get(("GET", "foo3")) == b"bar3"
     # add 1 more key to redis (exceed the max size)
     r.set("foo4", "bar4")
     assert r.get("foo4") == b"bar4"
     # test the eviction policy
-    assert len(r.client_cache.cache) == 3
-    assert r.client_cache.get(("GET", "foo")) == b"bar"
-    assert r.client_cache.get(("GET", "foo2")) is None
+    assert len(cache.cache) == 3
+    assert cache.get(("GET", "foo")) == b"bar"
+    assert cache.get(("GET", "foo2")) is None
+
+    r.flushdb()
 
 
 @pytest.mark.skipif(HIREDIS_AVAILABLE, reason="PythonParser only")
 def test_cache_decode_response():
-    r = redis.Redis(
-        decode_responses=True,
-        cache_enable=True,
-        single_connection_client=True,
-        protocol=3,
-    )
+    cache = _LocalCache()
+    r = redis.Redis(decode_responses=True, client_cache=cache, protocol=3)
     r.set("foo", "bar")
     # get key from redis and save in local cache
     assert r.get("foo") == "bar"
     # get key from local cache
-    assert r.client_cache.get(("GET", "foo")) == "bar"
+    assert cache.get(("GET", "foo")) == "bar"
     # change key in redis (cause invalidation)
     r.set("foo", "barbar")
     # send any command to redis (process invalidation in background)
     r.ping()
     # the command is not in the local cache anymore
-    assert r.client_cache.get(("GET", "foo")) is None
+    assert cache.get(("GET", "foo")) is None
     # get key from redis
     assert r.get("foo") == "barbar"
+
+    r.flushdb()
+
+
+@pytest.mark.skipif(HIREDIS_AVAILABLE, reason="PythonParser only")
+def test_cache_blacklist():
+    cache = _LocalCache()
+    r = redis.Redis(client_cache=cache, cache_blacklist=["LLEN"], protocol=3)
+    # add list to redis
+    r.lpush("mylist", "foo", "bar", "baz")
+    assert r.llen("mylist") == 3
+    assert r.lindex("mylist", 1) == b"bar"
+    assert cache.get(("LLEN", "mylist")) is None
+    assert cache.get(("LINDEX", "mylist", 1)) == b"bar"
+
+    r.flushdb()
