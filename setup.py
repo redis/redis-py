e90b7c5b60d0d8de1551d9996d5c4bb4ac4e222b
dvora-h <67596500+dvora-h@users.noreply.github.com>
2024-03-10 14:12:52 +0200

    Cherry pick for v5.0.3 (#3178)
    * Fix lock error (#3176)
    
    * Remove redundant async-timeout dependency in modern Python (#3177)
    
    https://github.com/redis/redis-py/issues/3174
    
    * Bump rojopolis/spellcheck-github-actions from 0.35.0 to 0.36.0 (#3172)
    
    Bumps [rojopolis/spellcheck-github-actions](https://github.com/rojopolis/spellcheck-github-actions) from 0.35.0 to 0.36.0.
    - [Release notes](https://github.com/rojopolis/spellcheck-github-actions/releases)
    - [Changelog](https://github.com/rojopolis/spellcheck-github-actions/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/rojopolis/spellcheck-github-actions/compare/0.35.0...0.36.0)
    
    ---
    updated-dependencies:
    - dependency-name: rojopolis/spellcheck-github-actions
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump release-drafter/release-drafter from 5 to 6 (#3171)
    
    Bumps [release-drafter/release-drafter](https://github.com/release-drafter/release-drafter) from 5 to 6.
    - [Release notes](https://github.com/release-drafter/release-drafter/releases)
    - [Commits](https://github.com/release-drafter/release-drafter/compare/v5...v6)
    
    ---
    updated-dependencies:
    - dependency-name: release-drafter/release-drafter
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Kamil Monicz <kamil@monicz.dev>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=[
         'importlib-metadata >= 1.0; python_version < "3.8"',
         'typing-extensions; python_version<"3.8"',
-        'async-timeout>=4.0.3',
+        'async-timeout>=4.0.3; python_full_version<"3.11.3"',
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
