21ef4dd3d38f76c15d67f4d27d998f145d6d19cc
Vladyslav Vildanov <117659936+vladvildanov@users.noreply.github.com>
2024-11-08 17:22:21 +0200

    Updated package version (#3426)
    * Updated package version
    
    * Updated docker-compose command
    
    * Updated asyncio version
    
    * Updated version range

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     long_description_content_type="text/markdown",
     keywords=["Redis", "key-value store", "database"],
     license="MIT",
-    version="5.0.8",
+    version="5.0.9",
     packages=find_packages(
         include=[
             "redis",
