1de69f590b7c0b50cbffceca35134bbabda56e44
Neil Bertram <neil@catalyst.net.nz>
2025-03-06 20:52:15 +1300

    Correct the typedef of lock.extend() to accept floats, and test that float TTLs are honoured precisely (#3420)

--- tests/test_lock.py
+++ tests/test_lock.py
@@ -178,11 +178,12 @@ class TestLock:
         lock.release()
 
     def test_extend_lock_float(self, r):
-        lock = self.get_lock(r, "foo", timeout=10.0)
+        lock = self.get_lock(r, "foo", timeout=10.5)
         assert lock.acquire(blocking=False)
-        assert 8000 < r.pttl("foo") <= 10000
-        assert lock.extend(10.0)
-        assert 16000 < r.pttl("foo") <= 20000
+        assert 10400 < r.pttl("foo") <= 10500
+        old_ttl = r.pttl("foo")
+        assert lock.extend(10.5)
+        assert old_ttl + 10400 < r.pttl("foo") <= old_ttl + 10500
         lock.release()
 
     def test_extending_unlocked_lock_raises_error(self, r):
