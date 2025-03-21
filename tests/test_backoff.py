9c530bb7cf392152ed388cca3ffb7f37e775f92b
jimcockburn <jim.cockburn@wayflyer.com>
2025-03-21 16:25:04 +0000

    formatting

--- tests/test_backoff.py
+++ tests/test_backoff.py
@@ -11,8 +11,8 @@ def test_exponential_with_jitter_backoff(monkeypatch: pytest.MonkeyPatch) -> Non
 
     bo = ExponentialWithJitterBackoff(cap=5, base=1)
 
-    assert bo.compute(0) == 0.25 # min(5, 0.25*2^0)
-    assert bo.compute(1) == 1.0 # min(5, 0.5*2^1)
-    assert bo.compute(2) == 3.0 # min(5, 0.75*2^2)
-    assert bo.compute(3) == 5.0 # min(5, 1*2^3)
-    assert bo.compute(4) == 5.0 # min(5, 0.9*2^4)
+    assert bo.compute(0) == 0.25  # min(5, 0.25*2^0)
+    assert bo.compute(1) == 1.0  # min(5, 0.5*2^1)
+    assert bo.compute(2) == 3.0  # min(5, 0.75*2^2)
+    assert bo.compute(3) == 5.0  # min(5, 1*2^3)
+    assert bo.compute(4) == 5.0  # min(5, 0.9*2^4)
