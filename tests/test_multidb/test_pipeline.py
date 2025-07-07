f16b6462f0fa1b15a0e7316c8bdd1571ebd7aa82
vladvildanov <vladyslav.vildanov@redis.com>
2025-07-07 12:52:23 +0300

    Added Pipeline testing

---
+++ tests/test_multidb/test_pipeline.py
@@ -0,0 +1,200 @@
+from time import sleep
+from unittest.mock import patch, Mock
+
+import pybreaker
+import pytest
+
+from redis.client import Pipeline
+from redis.multidb.circuit import State as CBState, PBCircuitBreakerAdapter
+from redis.multidb.client import MultiDBClient
+from redis.multidb.config import DEFAULT_HEALTH_CHECK_RETRIES, DEFAULT_HEALTH_CHECK_BACKOFF, DEFAULT_FAILOVER_RETRIES, \
+    DEFAULT_FAILOVER_BACKOFF
+from redis.multidb.failover import WeightBasedFailoverStrategy
+from redis.multidb.healthcheck import EchoHealthCheck
+from redis.retry import Retry
+from tests.test_multidb.conftest import create_weighted_list
+
+def mock_pipe() -> Pipeline:
+    mock_pipe = Mock(spec=Pipeline)
+    mock_pipe.__enter__ = Mock(return_value=mock_pipe)
+    mock_pipe.__exit__ = Mock(return_value=None)
+    return mock_pipe
+
+class TestPipeline:
+    @pytest.mark.parametrize(
+        'mock_multi_db_config,mock_db, mock_db1, mock_db2',
+        [
+            (
+                    {},
+                    {'weight': 0.2, 'circuit': {'state': CBState.CLOSED}},
+                    {'weight': 0.7, 'circuit': {'state': CBState.CLOSED}},
+                    {'weight': 0.5, 'circuit': {'state': CBState.CLOSED}},
+            ),
+        ],
+        indirect=True,
+    )
+    def test_executes_pipeline_against_correct_db(
+            self, mock_multi_db_config, mock_db, mock_db1, mock_db2
+    ):
+        databases = create_weighted_list(mock_db, mock_db1, mock_db2)
+
+        with patch.object(
+                mock_multi_db_config,
+                'databases',
+                return_value=databases
+        ):
+            pipe = mock_pipe()
+            pipe.execute.return_value = ['OK1', 'value1']
+            mock_db1.client.pipeline.return_value = pipe
+
+            for hc in mock_multi_db_config.health_checks:
+                hc.check_health.return_value = True
+
+            client = MultiDBClient(mock_multi_db_config)
+            assert mock_multi_db_config.failover_strategy.set_databases.call_count == 1
+
+            pipe = client.pipeline()
+            pipe.set('key1', 'value1')
+            pipe.get('key1')
+
+            assert pipe.execute() == ['OK1', 'value1']
+
+            for hc in mock_multi_db_config.health_checks:
+                assert hc.check_health.call_count == 3
+
+    @pytest.mark.parametrize(
+        'mock_multi_db_config,mock_db, mock_db1, mock_db2',
+        [
+            (
+                    {},
+                    {'weight': 0.2, 'circuit': {'state': CBState.CLOSED}},
+                    {'weight': 0.5, 'circuit': {'state': CBState.CLOSED}},
+                    {'weight': 0.7, 'circuit': {'state': CBState.OPEN}},
+            ),
+        ],
+        indirect=True,
+    )
+    def test_execute_pipeline_against_correct_db_and_closed_circuit(
+            self, mock_multi_db_config, mock_db, mock_db1, mock_db2
+    ):
+        databases = create_weighted_list(mock_db, mock_db1, mock_db2)
+
+        with patch.object(
+                mock_multi_db_config,
+                'databases',
+                return_value=databases
+        ):
+            pipe = mock_pipe()
+            pipe.execute.return_value = ['OK1', 'value1']
+            mock_db1.client.pipeline.return_value = pipe
+
+            for hc in mock_multi_db_config.health_checks:
+                hc.check_health.side_effect = [False, True, True]
+
+            client = MultiDBClient(mock_multi_db_config)
+            assert mock_multi_db_config.failover_strategy.set_databases.call_count == 1
+
+            with client.pipeline() as pipe:
+                pipe.set('key1', 'value1')
+                pipe.get('key1')
+
+            assert pipe.execute() == ['OK1', 'value1']
+
+            for hc in mock_multi_db_config.health_checks:
+                assert hc.check_health.call_count == 3
+
+            assert mock_db.circuit.state == CBState.CLOSED
+            assert mock_db1.circuit.state == CBState.CLOSED
+            assert mock_db2.circuit.state == CBState.OPEN
+
+    @pytest.mark.parametrize(
+        'mock_multi_db_config,mock_db, mock_db1, mock_db2',
+        [
+            (
+                    {},
+                    {'weight': 0.2, 'circuit': {'state': CBState.CLOSED}},
+                    {'weight': 0.7, 'circuit': {'state': CBState.CLOSED}},
+                    {'weight': 0.5, 'circuit': {'state': CBState.CLOSED}},
+            ),
+        ],
+        indirect=True,
+    )
+    def test_execute_pipeline_against_correct_db_on_background_health_check_determine_active_db_unhealthy(
+            self, mock_multi_db_config, mock_db, mock_db1, mock_db2
+    ):
+        cb = PBCircuitBreakerAdapter(pybreaker.CircuitBreaker(reset_timeout=5))
+        cb.database = mock_db
+        mock_db.circuit = cb
+
+        cb1 = PBCircuitBreakerAdapter(pybreaker.CircuitBreaker(reset_timeout=5))
+        cb1.database = mock_db1
+        mock_db1.circuit = cb1
+
+        cb2 = PBCircuitBreakerAdapter(pybreaker.CircuitBreaker(reset_timeout=5))
+        cb2.database = mock_db2
+        mock_db2.circuit = cb2
+
+        databases = create_weighted_list(mock_db, mock_db1, mock_db2)
+
+        with patch.object(
+                mock_multi_db_config,
+                'databases',
+                return_value=databases
+        ):
+            mock_db.client.execute_command.side_effect = ['healthcheck', 'healthcheck', 'healthcheck', 'error']
+            mock_db1.client.execute_command.side_effect = ['healthcheck', 'error', 'error', 'healthcheck']
+            mock_db2.client.execute_command.side_effect = ['healthcheck', 'healthcheck', 'error', 'error']
+
+            pipe = mock_pipe()
+            pipe.execute.return_value = ['OK', 'value']
+            mock_db.client.pipeline.return_value = pipe
+
+            pipe1 = mock_pipe()
+            pipe1.execute.return_value = ['OK1', 'value']
+            mock_db1.client.pipeline.return_value = pipe1
+
+            pipe2 = mock_pipe()
+            pipe2.execute.return_value = ['OK2', 'value']
+            mock_db2.client.pipeline.return_value = pipe2
+
+            mock_multi_db_config.health_check_interval = 0.1
+            mock_multi_db_config.health_checks = [
+                EchoHealthCheck(
+                    retry=Retry(retries=DEFAULT_HEALTH_CHECK_RETRIES, backoff=DEFAULT_HEALTH_CHECK_BACKOFF)
+                )
+            ]
+            mock_multi_db_config.failover_strategy = WeightBasedFailoverStrategy(
+                retry=Retry(retries=DEFAULT_FAILOVER_RETRIES, backoff=DEFAULT_FAILOVER_BACKOFF)
+            )
+
+            client = MultiDBClient(mock_multi_db_config)
+
+            with client.pipeline() as pipe:
+                pipe.set('key1', 'value')
+                pipe.get('key1')
+
+            assert pipe.execute() == ['OK1', 'value']
+
+            sleep(0.15)
+
+            with client.pipeline() as pipe:
+                pipe.set('key1', 'value')
+                pipe.get('key1')
+
+            assert pipe.execute() == ['OK2', 'value']
+
+            sleep(0.1)
+
+            with client.pipeline() as pipe:
+                pipe.set('key1', 'value')
+                pipe.get('key1')
+
+            assert pipe.execute() == ['OK', 'value']
+
+            sleep(0.1)
+
+            with client.pipeline() as pipe:
+                pipe.set('key1', 'value')
+                pipe.get('key1')
+
+            assert pipe.execute() == ['OK1', 'value']
