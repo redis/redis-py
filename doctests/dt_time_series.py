f0dfa02b04c57d4042f08af1395342693dde4082
Andy Stark <andrew.stark@redis.com>
2025-07-10 11:27:12 +0100

    DOC-5424 creation and query examples

---
+++ doctests/dt_time_series.py
@@ -0,0 +1,280 @@
+# EXAMPLE: time_series_tutorial
+# HIDE_START
+"""
+Code samples for time series page:
+    https://redis.io/docs/latest/develop/data-types/timeseries/
+"""
+
+import redis
+
+r = redis.Redis(decode_responses=True)
+# HIDE_END
+
+# REMOVE_START
+r.delete(
+    "thermometer:1", "thermometer:2", "thermometer:3",
+    "rg:1", "rg:2", "rg:3", "rg:4",
+    "sensor:3",
+    "wind:1", "wind:2", "wind:3", "wind:4",
+    "hyg:1", "hyg:compacted"
+)
+# REMOVE_END
+
+# STEP_START create
+res1 = r.ts().create("thermometer:1")
+print(res1)  # >>> True
+
+res2 = r.type("thermometer:1")
+print(res2)  # >>> TSDB-TYPE
+
+res3 = r.ts().info("thermometer:1")
+print(res3)
+# >>> {'rules': [], ... 'total_samples': 0, ...
+# STEP_END
+# REMOVE_START
+assert res1 is True
+assert res2 == "TSDB-TYPE"
+assert res3["total_samples"] == 0
+# REMOVE_END
+
+# STEP_START create_retention
+# Note: the parameter name is `retention_msecs`, but you
+# can interpret the time unit however you like. Here, we
+# assume 100 days.
+res4 = r.ts().add("thermometer:2", 1, 10.8, retention_msecs=100)
+print(res4)  # >>> 1
+
+res5 = r.ts().info("thermometer:2")
+print(res5)
+# >>> {'rules': [], ... 'retention_msecs': 100, ...
+# STEP_END
+# REMOVE_START
+assert res4 == 1
+assert res5["retention_msecs"] == 100
+# REMOVE_END
+
+# STEP_START create_labels
+res6 = r.ts().create(
+    "thermometer:3", 1, 10.4,
+    labels={"location": "UK", "type": "Mercury"}
+)
+print(res6)  # >>> 1
+
+res7 = r.ts().info("thermometer:3")
+print(res7)
+# >>> {'rules': [], ... 'labels': {'location': 'UK', 'type': 'Mercury'}, ...
+# STEP_END
+# REMOVE_START
+assert res6 == 1
+assert res7["labels"] == {"location": "UK", "type": "Mercury"}
+# REMOVE_END
+
+# STEP_START madd
+res8 = r.ts().madd([
+    ("thermometer:1", 1, 9.2),
+    ("thermometer:1", 2, 9.9),
+    ("thermometer:2", 2, 10.3)
+])
+print(res8)  # >>> [1, 2, 2]
+# STEP_END
+# REMOVE_START
+assert res8 == [1, 2, 2]
+# REMOVE_END
+
+# STEP_START get
+# The last recorded temperature for thermometer:2
+# was 10.3 on day 2.
+res9 = r.ts().get("thermometer:2")
+print(res9)  # >>> (2, 10.3)
+# STEP_END
+# REMOVE_START
+assert res9 == (2, 10.3)
+# REMOVE_END
+
+# STEP_START range
+# Add 5 data points to a rain gauge time series.
+res10 = r.ts().create("rg:1")
+print(res10)  # >>> True
+
+res11 = r.ts().madd([
+        ("rg:1", 0, 18),
+        ("rg:1", 1, 14),
+        ("rg:1", 2, 22),
+        ("rg:1", 3, 18),
+        ("rg:1", 4, 24),
+])
+print(res11)  # >>> [0, 1, 2, 3, 4]
+
+# Retrieve all the data points in ascending order.
+res12 = r.ts().range("rg:1", "-", "+")
+print(res12)  # >>> [(0, 18.0), (1, 14.0), (2, 22.0), (3, 18.0), (4, 24.0)]
+
+# Retrieve data points up to day 1 (inclusive).
+res13 = r.ts().range("rg:1", "-", 1)
+print(res13)  # >>> [(0, 18.0), (1, 14.0)]
+
+
+# Retrieve data points from day 3 onwards.
+res14 = r.ts().range("rg:1", 3, "+")
+print(res14)  # >>> [(3, 18.0), (4, 24.0)]
+
+# Retrieve all the data points in descending order.
+res15 = r.ts().revrange("rg:1", "-", "+")
+print(res15)  # >>> [(4, 24.0), (3, 18.0), (2, 22.0), (1, 14.0), (0, 18.0)]
+
+# Retrieve data points up to day 1 (inclusive), but return them
+# in descending order.
+res16 = r.ts().revrange("rg:1", "-", 1)
+print(res16)  # >>> [(1, 14.0), (0, 18.0)]
+# STEP_END
+# REMOVE_START
+assert res10 is True
+assert res11 == [0, 1, 2, 3, 4]
+assert res12 == [(0, 18.0), (1, 14.0), (2, 22.0), (3, 18.0), (4, 24.0)]
+assert res13 == [(0, 18.0), (1, 14.0)]
+assert res14 == [(3, 18.0), (4, 24.0)]
+assert res15 == [(4, 24.0), (3, 18.0), (2, 22.0), (1, 14.0), (0, 18.0)]
+assert res16 == [(1, 14.0), (0, 18.0)]
+# REMOVE_END
+
+# STEP_START range_filter
+res17 = r.ts().range("rg:1", "-", "+", filter_by_ts=[0, 2, 4])
+print(res17)  # >>> [(0, 18.0), (2, 22.0), (4, 24.0)]
+
+res18 = r.ts().revrange(
+    "rg:1", "-", "+",
+    filter_by_ts=[0, 2, 4],
+    filter_by_min_value=20,
+    filter_by_max_value=25,
+)
+print(res18)  # >>> [(4, 24.0), (2, 22.0)]
+
+res19 = r.ts().revrange(
+    "rg:1", "-", "+",
+    filter_by_ts=[0, 2, 4],
+    filter_by_min_value=22,
+    filter_by_max_value=22,
+    count=1,
+)
+print(res19)  # >>> [(2, 22.0)]
+# STEP_END
+# REMOVE_START
+assert res17 == [(0, 18.0), (2, 22.0), (4, 24.0)]
+assert res18 == [(4, 24.0), (2, 22.0)]
+assert res19 == [(2, 22.0)]
+# REMOVE_END
+
+# STEP_START query_multi
+# Create three new rain gauge time series, two in the US
+# and one in the UK, with different units and add some
+# data points.
+res20 = r.ts().create(
+    "rg:2",
+    labels={"location": "us", "unit": "cm"},
+)
+print(res20)  # >>> True
+
+res21 = r.ts().create(
+    "rg:3",
+    labels={"location": "us", "unit": "in"},
+)
+print(res21)  # >>> True
+
+res22 = r.ts().create(
+    "rg:4",
+    labels={"location": "uk", "unit": "mm"},
+)
+print(res22)  # >>> True
+
+res23 = r.ts().madd([
+        ("rg:2", 0, 1.8),
+        ("rg:3", 0, 0.9),
+        ("rg:4", 0, 25.4),
+])
+print(res23)  # >>> [0, 0, 0]
+
+res24 = r.ts().madd([
+        ("rg:2", 1, 2.1),
+        ("rg:3", 1, 0.77),
+        ("rg:4", 1, 18),
+])
+print(res24)  # >>> [1, 1, 1]
+
+res25 = r.ts().madd([
+        ("rg:2", 2, 2.3),
+        ("rg:3", 2, 1.1),
+        ("rg:4", 2, 21),
+])
+print(res25)  # >>> [2, 2, 2]
+
+res26 = r.ts().madd([
+        ("rg:2", 3, 1.9),
+        ("rg:3", 3, 0.81),
+        ("rg:4", 3, 19),
+])
+print(res26)  # >>> [3, 3, 3]
+
+res27 = r.ts().madd([
+        ("rg:2", 4, 1.78),
+        ("rg:3", 4, 0.74),
+        ("rg:4", 4, 23),
+])
+print(res27)  # >>> [4, 4, 4]
+
+# Retrieve the last data point from each US rain gauge. If
+# you don't specify any labels, an empty array is returned
+# for the labels.
+res28 = r.ts().mget(["location=us"])
+print(res28)  # >>> [{'rg:2': [{}, 4, 1.78]}, {'rg:3': [{}, 4, 0.74]}]
+
+# Retrieve the same data points, but include the `unit`
+# label in the results.
+res29 = r.ts().mget(["location=us"], select_labels=["unit"])
+print(res29)  # >>> [{'unit': 'cm'}, (4, 1.78), {'unit': 'in'}, (4, 0.74)]
+
+# Retrieve data points up to day 2 (inclusive) from all
+# rain gauges that report in millimeters. Include all
+# labels in the results.
+res30 = r.ts().mrange(
+    "-", 2, filters=["unit=mm"], with_labels=True
+)
+print(res30)
+# >>> [{'rg:4': [{'location': 'uk', 'unit': 'mm'}, [(0, 25.4),...
+
+# Retrieve data points from day 1 to day 3 (inclusive) from
+# all rain gauges that report in centimeters or millimeters,
+# but only return the `location` label. Return the results
+# in descending order of timestamp.
+res31 = r.ts().mrevrange(
+    1, 3, filters=["unit=(cm,mm)"], select_labels=["location"]
+)
+print(res31)
+# >>> [[{'location': 'uk'}, (3, 19.0), (2, 21.0), (1, 18.0)],...
+# STEP_END
+# REMOVE_START
+assert res20 is True
+assert res21 is True
+assert res22 is True
+assert res23 == [0, 0, 0]
+assert res24 == [1, 1, 1]
+assert res25 == [2, 2, 2]
+assert res26 == [3, 3, 3]
+assert res27 == [4, 4, 4]
+assert res28 == [{'rg:2': [{}, 4, 1.78]}, {'rg:3': [{}, 4, 0.74]}]
+assert res29 == [
+    {'rg:2': [{'unit': 'cm'}, 4, 1.78]},
+    {'rg:3': [{'unit': 'in'}, 4, 0.74]}
+]
+assert res30 == [
+    {
+        'rg:4': [
+            {'location': 'uk', 'unit': 'mm'},
+            [(0, 25.4), (1, 18.0), (2, 21.0)]
+        ]
+    }
+]
+assert res31 == [
+    {'rg:2': [{'location': 'us'}, [(3, 1.9), (2, 2.3), (1, 2.1)]]},
+    {'rg:4': [{'location': 'uk'}, [(3, 19.0), (2, 21.0), (1, 18.0)]]}
+]
+# REMOVE_END
