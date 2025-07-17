f64e10d188b79676c7cc390279e2b211f63c13ed
vladvildanov <vladyslav.vildanov@redis.com>
2025-07-17 11:21:09 +0300

    Added support for Pub/Sub in MultiDBClient

--- redis/multidb/command_executor.py
+++ redis/multidb/command_executor.py
@@ -3,13 +3,13 @@ from abc import ABC, abstractmethod
 from datetime import datetime, timedelta
 from typing import List, Union, Optional, Callable
 
-from redis.client import Pipeline
+from redis.client import Pipeline, PubSub, PubSubWorkerThread
 from redis.exceptions import ConnectionError, TimeoutError
 from redis.event import EventDispatcherInterface, OnCommandsFailEvent
 from redis.multidb.config import DEFAULT_AUTO_FALLBACK_INTERVAL
 from redis.multidb.database import Database, AbstractDatabase, Databases
 from redis.multidb.circuit import State as CBState
-from redis.multidb.event import RegisterCommandFailure
+from redis.multidb.event import RegisterCommandFailure, ActiveDatabaseChanged, ResubscribeOnActiveDatabaseChanged
 from redis.multidb.failover import FailoverStrategy
 from redis.multidb.failure_detector import FailureDetector
 
@@ -35,7 +35,7 @@ class CommandExecutor(ABC):
 
     @property
     @abstractmethod
-    def active_database(self) -> Union[Database, None]:
+    def active_database(self) -> Optional[Database]:
         """Returns currently active database."""
         pass
 
@@ -43,6 +43,23 @@ class CommandExecutor(ABC):
     @abstractmethod
     def active_database(self, database: AbstractDatabase) -> None:
         """Sets currently active database."""
+        pass
+
+    @abstractmethod
+    def pubsub(self, **kwargs):
+        """Initializes a PubSub object on a currently active database"""
+        pass
+
+    @property
+    @abstractmethod
+    def active_pubsub(self) -> Optional[PubSub]:
+        """Returns currently active pubsub."""
+        pass
+
+    @active_pubsub.setter
+    @abstractmethod
+    def active_pubsub(self, pubsub: PubSub) -> None:
+        """Sets currently active pubsub."""
         pass
 
     @property
@@ -93,7 +110,9 @@ class DefaultCommandExecutor(CommandExecutor):
         self._event_dispatcher = event_dispatcher
         self._auto_fallback_interval = auto_fallback_interval
         self._next_fallback_attempt: datetime
-        self._active_database: Union[Database, None] = None
+        self._active_database: Optional[Database] = None
+        self._active_pubsub: Optional[PubSub] = None
+        self._active_pubsub_kwargs = {}
         self._setup_event_dispatcher()
         self._schedule_next_fallback()
 
@@ -114,7 +133,21 @@ class DefaultCommandExecutor(CommandExecutor):
 
     @active_database.setter
     def active_database(self, database: AbstractDatabase) -> None:
+        old_active = self._active_database
         self._active_database = database
+
+        if old_active is not None and old_active is not database:
+            self._event_dispatcher.dispatch(
+                ActiveDatabaseChanged(old_active, self._active_database, self, **self._active_pubsub_kwargs)
+            )
+
+    @property
+    def active_pubsub(self) -> Optional[PubSub]:
+        return self._active_pubsub
+
+    @active_pubsub.setter
+    def active_pubsub(self, pubsub: PubSub) -> None:
+        self._active_pubsub = pubsub
 
     @property
     def failover_strategy(self) -> FailoverStrategy:
@@ -157,6 +190,39 @@ class DefaultCommandExecutor(CommandExecutor):
 
         return self._execute_with_failure_detection(callback)
 
+    def pubsub(self, **kwargs):
+        def callback(database):
+            if self._active_pubsub is None:
+                self._active_pubsub = database.client.pubsub(**kwargs)
+                self._active_pubsub_kwargs = kwargs
+            return None
+
+        return self._execute_with_failure_detection(callback)
+
+    def execute_pubsub_method(self, method_name: str, *args, **kwargs):
+        """
+        Executes given method on active pub/sub.
+        """
+        def callback(database):
+            method = getattr(self.active_pubsub, method_name)
+            return method(*args, **kwargs)
+
+        return self._execute_with_failure_detection(callback, *args)
+
+    def execute_pubsub_run_in_thread(
+        self,
+        pubsub,
+        sleep_time: float = 0.0,
+        daemon: bool = False,
+        exception_handler: Optional[Callable] = None,
+    ) -> "PubSubWorkerThread":
+        def callback(database):
+            return self._active_pubsub.run_in_thread(
+                sleep_time, daemon=daemon, exception_handler=exception_handler, pubsub=pubsub
+            )
+
+        return self._execute_with_failure_detection(callback)
+
     def _execute_with_failure_detection(self, callback: Callable, cmds: tuple = ()):
         """
         Execute a commands execution callback with failure detection.
@@ -169,7 +235,7 @@ class DefaultCommandExecutor(CommandExecutor):
                     and self._next_fallback_attempt <= datetime.now()
                 )
         ):
-            self._active_database = self._failover_strategy.database
+            self.active_database = self._failover_strategy.database
             self._schedule_next_fallback()
 
         try:
@@ -189,9 +255,11 @@ class DefaultCommandExecutor(CommandExecutor):
 
     def _setup_event_dispatcher(self):
         """
-        Registers command failure event listener.
+        Registers necessary listeners.
         """
-        event_listener = RegisterCommandFailure(self._failure_detectors, self._databases)
+        failure_listener = RegisterCommandFailure(self._failure_detectors, self._databases)
+        resubscribe_listener = ResubscribeOnActiveDatabaseChanged()
         self._event_dispatcher.register_listeners({
-            OnCommandsFailEvent: [event_listener],
+            OnCommandsFailEvent: [failure_listener],
+            ActiveDatabaseChanged: [resubscribe_listener],
         })
--- redis/multidb/event.py
+++ redis/multidb/event.py
@@ -2,8 +2,57 @@ from typing import List
 
 from redis.event import EventListenerInterface, OnCommandsFailEvent
 from redis.multidb.config import Databases
+from redis.multidb.database import AbstractDatabase
 from redis.multidb.failure_detector import FailureDetector
 
+class ActiveDatabaseChanged:
+    """
+    Event fired when an active database has been changed.
+    """
+    def __init__(
+            self,
+            old_database: AbstractDatabase,
+            new_database: AbstractDatabase,
+            command_executor,
+            **kwargs
+    ):
+        self._old_database = old_database
+        self._new_database = new_database
+        self._command_executor = command_executor
+        self._kwargs = kwargs
+
+    @property
+    def old_database(self) -> AbstractDatabase:
+        return self._old_database
+
+    @property
+    def new_database(self) -> AbstractDatabase:
+        return self._new_database
+
+    @property
+    def command_executor(self):
+        return self._command_executor
+
+    @property
+    def kwargs(self):
+        return self._kwargs
+
+class ResubscribeOnActiveDatabaseChanged(EventListenerInterface):
+    """
+    Re-subscribe currently active pub/sub to a new active database.
+    """
+    def listen(self, event: ActiveDatabaseChanged):
+        old_pubsub = event.command_executor.active_pubsub
+
+        if old_pubsub is not None:
+            # Re-assign old channels and patterns so they will be automatically subscribed on connection.
+            new_pubsub = event.new_database.client.pubsub(**event.kwargs)
+            new_pubsub.channels = old_pubsub.channels
+            new_pubsub.patterns = old_pubsub.patterns
+            new_pubsub.shard_channels = old_pubsub.shard_channels
+            new_pubsub.on_connect(None)
+            event.command_executor.active_pubsub = new_pubsub
+            old_pubsub.close()
 
 class RegisterCommandFailure(EventListenerInterface):
     """
