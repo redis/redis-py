ff21998782ba474e786644ae8cffa3392183e6d5
petyaslavova <petya.slavova@redis.com>
2025-07-23 18:20:50 +0300

    Adding information in connection pool class doc string for available SSLConnection class, that can be used for SSL connections (#3710)

--- redis/asyncio/connection.py
+++ redis/asyncio/connection.py
@@ -1042,6 +1042,7 @@ class ConnectionPool:
     By default, TCP connections are created unless ``connection_class``
     is specified. Use :py:class:`~redis.UnixDomainSocketConnection` for
     unix sockets.
+    :py:class:`~redis.SSLConnection` can be used for SSL enabled connections.
 
     Any additional keyword arguments are passed to the constructor of
     ``connection_class``.
