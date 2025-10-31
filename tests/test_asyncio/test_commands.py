4050c91276b3202cd401628820f776975ee91fe5
Petya Slavova <petya.slavova@redis.com>
2025-10-31 19:45:48 +0200

    Fix async tests - await was missing

--- tests/test_asyncio/test_commands.py
+++ tests/test_asyncio/test_commands.py
@@ -3688,7 +3688,7 @@ class TestRedisCommands:
         await r.xadd(stream_2, {"key_m6": "val_m6"})
 
         # read all the messages - this will save the msgs in PEL
-        r.xreadgroup(group, consumer_1, streams={stream_1: ">", stream_2: ">"})
+        await r.xreadgroup(group, consumer_1, streams={stream_1: ">", stream_2: ">"})
 
         # add 2 more messages
         m7 = await r.xadd(stream_1, {"key_m7": "val_m7"})
