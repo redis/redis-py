330d2dd35f4af79eecbc3f590e566f775a600b3c
Samuel Guilhem-Ducleon <samuel@elevenlabs.io>
2025-12-09 15:41:34 -0500

    add test

--- tests/test_asyncio/test_sentinel_managed_connection.py
+++ tests/test_asyncio/test_sentinel_managed_connection.py
@@ -36,3 +36,29 @@ async def test_connect_retry_on_timeout_error(connect_args):
         assert mock_super_connect.await_count == 3
         assert connection_pool.get_master_address.call_count == 3
         await conn.disconnect()
+
+async def test_connect_check_health_retry_on_timeout_error(connect_args):
+    """Test that the _connect function is retried in case of a timeout"""
+    connection_pool = mock.AsyncMock()
+    connection_pool.get_master_address = mock.AsyncMock(
+        return_value=(connect_args["host"], connect_args["port"])
+    )
+    conn = SentinelManagedConnection(
+        retry_on_timeout=True,
+        retry=Retry(NoBackoff(), 3),
+        connection_pool=connection_pool,
+    )
+    original_super_connect = Connection._connect.__get__(conn, Connection)
+
+    with mock.patch.object(Connection, "_connect", new_callable=mock.AsyncMock) as mock_super_connect:
+        async def side_effect(*args, **kwargs):
+            if mock_super_connect.await_count <= 2:
+                raise socket.timeout()
+            return await original_super_connect(*args, **kwargs)
+
+        mock_super_connect.side_effect = side_effect
+
+        await conn.connect_check_health()
+        assert mock_super_connect.await_count == 3
+        assert connection_pool.get_master_address.call_count == 3
+        await conn.disconnect()
