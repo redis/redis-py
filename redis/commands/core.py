redis/redis-py

commit 028245c73dc0d3e86ef200d3e23af9112939b1c8

Update ping command docstrings and method return type hint. (#3789)

redis/commands/core.py
@@ -1210,11 +1210,18 @@ class ManagementCommands(CommandsProtocol):
         """
         return self.execute_command("LATENCY RESET", *events)
 
-    def ping(self, **kwargs) -> ResponseT:
+    def ping(self, **kwargs) -> Union[Awaitable[bool], bool]:
         """
-        Ping the Redis server
+        Ping the Redis server to test connectivity.
 
-        For more information, see https://redis.io/commands/ping
+        Sends a PING command to the Redis server and returns True if the server
+        responds with "PONG".
+
+        This command is useful for:
+        - Testing whether a connection is still alive
+        - Verifying the server's ability to serve data
+
+        For more information on the underlying ping command see https://redis.io/commands/ping
         """
         return self.execute_command("PING", **kwargs)
 

redis/asyncio/client.py
@@ -1161,9 +1161,12 @@ class PubSub:
             return await self.handle_message(response, ignore_subscribe_messages)
         return None
 
-    def ping(self, message=None) -> Awaitable:
+    def ping(self, message=None) -> Awaitable[bool]:
         """
-        Ping the Redis server
+        Ping the Redis server to test connectivity.
+
+        Sends a PING command to the Redis server and returns True if the server
+        responds with "PONG".
         """
         args = ["PING", message] if message is not None else ["PING"]
         return self.execute_command(*args)

redis/client.py
@@ -1186,7 +1186,10 @@ class PubSub:
 
     def ping(self, message: Union[str, None] = None) -> bool:
         """
-        Ping the Redis server
+        Ping the Redis server to test connectivity.
+
+        Sends a PING command to the Redis server and returns True if the server
+        responds with "PONG".
         """
         args = ["PING", message] if message is not None else ["PING"]
         return self.execute_command(*args)

commit f37f7753c987da7b4920b803a6af54951985e39c

Fix async ping return type hint regression
Add async override for ping() method in AsyncManagementCommands to fix
type checking error introduced in PR #3789. The base ManagementCommands.ping()
returns Union[Awaitable[bool], bool] which causes type checkers to incorrectly
flag await r.ping() as a type error for async clients.

The fix adds an explicit async override that returns bool (which becomes
Awaitable[bool] due to async def), ensuring type checkers understand that
the async client's ping() always returns an awaitable.

Fixes #3920

redis/commands/core.py
@@ -1513,6 +1513,17 @@ class AsyncManagementCommands(ManagementCommands):
     async def memory_help(self, **kwargs) -> None:
         return super().memory_help(**kwargs)
 
+    async def ping(self, **kwargs) -> bool:
+        """
+        Ping the Redis server to test connectivity.
+
+        Sends a PING command to the Redis server and returns True if the server
+        responds with "PONG".
+
+        For more information, see https://redis.io/commands/ping
+        """
+        return await self.execute_command("PING", **kwargs)
+
     async def shutdown(
         self,
         save: bool = False,