50c101320a2d255fe446c390259db1538e3acb3d
Robert Rollins <rrollins@caltech.edu>
2026-04-22 17:10:50 -0700

    Added asyncio version of ssl_min_version parser

--- redis/asyncio/connection.py
+++ redis/asyncio/connection.py
@@ -1170,6 +1170,7 @@ URL_QUERY_ARGUMENT_PARSERS: Mapping[str, Callable[..., object]] = MappingProxyTy
         "ssl_check_hostname": to_bool,
         "ssl_include_verify_flags": parse_ssl_verify_flags,
         "ssl_exclude_verify_flags": parse_ssl_verify_flags,
+        "ssl_min_version": int,
         "timeout": float,
     }
 )
