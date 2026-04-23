7bd78bc5126f6b465f4baf476afa6b8c1b65cbd2
Robert Rollins <rrollins@caltech.edu>
2026-04-23 14:10:22 -0700

    Added tests

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,6 +1,7 @@
 import copy
 import platform
 import socket
+import ssl
 import sys
 import threading
 import types
@@ -339,6 +340,14 @@ def test_redis_from_pool(request, from_url):
 
     assert called == 1
     pool.disconnect()
+
+
+@pytest.mark.fixed_client
+def test_create_secure_client_from_url_with_minimum_ssl_version():
+    client = redis.Redis.from_url(
+        f"rediss://localhost:6379/0?ssl_cert_reqs=none&ssl_min_version={ssl.TLSVersion.TLSv1_3}"
+    )
+    assert client.connection_pool.connection_kwargs["ssl_min_version"] == ssl.TLSVersion.TLSv1_3
 
 
 @pytest.mark.parametrize(
